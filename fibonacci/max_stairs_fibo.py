2fa23093041f9a069fdaa5216c4675954f512104
soaringwave <himhim2514@gmail.com>
2022-07-23 13:38:51 +0900

    Update max_stairs_fibo.py
    + ref

--- fibonacci/max_stairs_fibo.py
+++ fibonacci/max_stairs_fibo.py
@@ -1,11 +1,23 @@
 # 2579
+# 계단 오르기
+# 참고(https://sungmin-joo.tistory.com/18)
 
 import sys
 
 n = int(input())
 stairs = []
+max_sum = []
 for _ in range(n):
     stairs.append(int(sys.stdin.readline()))
 
+# 인덱스 0부터 시작
+max_sum.append(stairs[0])
+if n > 1: # OutOfIndex 방지
+    max_sum.append(stairs[0] + stairs[1])
+if n > 2:
+    max_sum.append(max(stairs[0], stairs[1]) + stairs[2])
+if n > 3:
+    for i in range(3, n):
+        max_sum.append(max(stairs[i - 1] + max_sum[i - 3], max_sum[i - 2]) + stairs[i])
 
-
+print(max_sum[n - 1])
