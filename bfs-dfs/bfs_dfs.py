896d8fa0ca5abd6fae38556767a6c4f9d14f725d
soaringwave <himhim2514@gmail.com>
2022-08-02 23:36:42 +0900

    Update bfs_dfs.py
    Fixing

--- bfs-dfs/bfs_dfs.py
+++ bfs-dfs/bfs_dfs.py
@@ -1,6 +1,39 @@
 # 1260
 # dfs와 bfs
 
-# 정점, 간선, 시작 정점
-n, m, v = int(input().split())
+import sys
+sys.setrecursionlimit(10 ** 6)
+from collections import deque
 
+# 정점, 간선, 시작 정점
+n, m, r = map(int, sys.stdin.readline().split())
+
+visited = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    num1, num2 = map(int, sys.stdin.readline().split())
+    # 무방향 그래프
+    graph[num1].append(num2)
+    graph[num2].append(num1)
+
+for i in range(n + 1):
+    graph[i].sort()
+
+
+def dfs(e, r, visited):
+    visited[r] = 1
+    print(r, end=' ')
+    for i in e[r]:
+        if visited[i] == 0:
+            dfs(e, i, visited)
+
+
+def bfs(e, r, visited):
+    visited[r] = 1
+    print(r, end='')
+    queue = deque(e[r])
+
+
+dfs(graph, r, visited)
+print()
