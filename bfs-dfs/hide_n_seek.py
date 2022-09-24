e1b5fc854c310613b3d58aa1183af94286868646
soaringwave <himhim2514@gmail.com>
2022-09-24 19:26:52 +0900

    Create hide_n_seek.py
    Making simple algorithm

---
+++ bfs-dfs/hide_n_seek.py
@@ -0,0 +1,17 @@
+# 1697
+# 숨바꼭질
+
+n, k = map(int, input().split())
+
+count = 0
+
+def go(start, end):
+    global count
+    if start == end:
+        return count
+    start += 1
+    start -= 1
+    start *= 2
+    count += 1
+
+print(count)
