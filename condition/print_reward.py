6923ffcc86ee5db63a7850f8f8f61335ce124d96
soaringwave <himhim2514@gmail.com>
2022-09-27 23:55:42 +0900

    Update print_reward.py
    Finishing condition algorithm

--- condition/print_reward.py
+++ condition/print_reward.py
@@ -19,7 +19,17 @@ if num1 == num3:
 
 # 같은 눈이 3개 나오는 경우
 if count == 3:
-    reward = 10000 +
+    reward = 10000 + same_num * 1000
 # 같은 눈이 2개 나오는 경우
-
+if count == 1:
+    reward = 1000 + same_num * 100
 # 같은 눈이 없는 경우
+if count == 0:
+    max = num1
+    if max < num2:
+        max = num2
+    if max < num3:
+        max = num3
+    reward = max * 100
+
+print(reward)
