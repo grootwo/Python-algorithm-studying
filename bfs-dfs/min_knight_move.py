149f98c91ba58a21cb3aab466f26e27e5a0e55ac
soaringwave <himhim2514@gmail.com>
2022-10-04 21:55:42 +0900

    Update min_knight_move.py
    Edit condition in short way

--- bfs-dfs/min_knight_move.py
+++ bfs-dfs/min_knight_move.py
@@ -7,8 +7,6 @@ test_case = int(input())
 kx = [1, 1, -1, -1, 2, 2, -2, -2] # knight의 x 이동 범위
 ky = [2, -2, 2, -2, 1, -1, 1, -1] # knight의 y 이동 범위
 
-# square를 어디에 정의하지?
-
 def bfs(x1, y1, x2, y2, square):
     queue = deque()
     queue.append((x1, y1))
@@ -17,22 +15,12 @@ def bfs(x1, y1, x2, y2, square):
         for i in range(8):
             nx = x + kx[i]
             ny = y + ky[i]
-            # 만약 인덱스를 벗어난다면
-            if nx < 0 or nx >= square or ny < 0 or ny >= square:
-                continue
-            else:
-                # 만약 처음 방문한다면
-                if graph[nx][ny] == None:
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append((nx, ny))
-                # 만약 길이라면
-                else:
-                    if graph[nx][ny] < graph[x][y] + 1:
-                        graph[nx][ny] = graph[x][y] + 1
-                        queue.append((nx, ny))
-                    else:
-                        continue
-    return graph[x2][y2]
+            # 체스판 안이고, 방문하지 않았다면
+            if 0 <= nx < square and 0 <= ny < square and graph[nx][ny] == None:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+            if graph[x2][y2] != None:
+                return graph[x2][y2]
 
 
 for i in range(test_case):
