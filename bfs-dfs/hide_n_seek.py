fb7810278b5da8895e16a5d8d7e5777914cfb4a8
soaringwave <himhim2514@gmail.com>
2022-10-04 13:34:55 +0900

    Update hide_n_seek.py
    Edit by using former bfs algorithm(min_maze/py)

--- bfs-dfs/hide_n_seek.py
+++ bfs-dfs/hide_n_seek.py
@@ -1,17 +1,44 @@
 # 1697
 # 숨바꼭질
+from collections import deque
 
-now, end = map(int, input().split())
+start, end = map(int, input().split())
 
-count = 0
+if end > start:
+    index = end * 2
+else:
+    index = start * 2
 
-def go(start, end):
-    global count
-    if start == end:
-        return count
-    start += 1
-    start -= 1
-    start *= 2
-    count += 1
+graph = [None] * (index + 1)
+graph[start] = 0
 
-print(count)
+
+def bfs(now, end):
+    queue = deque()
+    queue.append(now)
+    while queue:
+        now = queue.popleft()
+        for i in range(3):
+            if i == 0:
+                next = now * 2
+            elif i == 1:
+                next = now + 1
+            else:
+                next = now - 1
+            # 만약 인덱스를 벗어난다면
+            if next > end * 2 or next < 0:
+                continue
+            # 만약 처음 방문한다면
+            if graph[next] == None:
+                graph[next] = graph[now] + 1
+                queue.append(next)
+            # 이미 방문했던 곳이라면
+            else:
+                # 최소의 값을 입력
+                if graph[next] > (graph[now] + 1):
+                    graph[next] = graph[now] + 1
+                    queue.append(next)
+    return graph[end]
+
+
+print(bfs(start, end))
