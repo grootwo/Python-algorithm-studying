c840ca03e6876183dea96d6b709cf65aa3bdf6c6
soaringwave <himhim2514@gmail.com>
2022-10-06 23:09:04 +0900

    Create ladder_game.py
    Write input part

---
+++ bfs-dfs/ladder_game.py
@@ -0,0 +1,9 @@
+# 16928
+# 뱀과 사다리 게임
+
+ladder_count, snake_count = map(int, input().split())
+map_dic = {}
+
+for i in range(ladder_count):
+    num1, num2 = map(int, input().split())
+    map_dic[]
