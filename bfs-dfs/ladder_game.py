e6e79218f31494dbf3fd9d1ce480ff32ae79c107
soaringwave <himhim2514@gmail.com>
2022-10-07 22:54:26 +0900

    Update ladder_game.py
    Add input part

--- bfs-dfs/ladder_game.py
+++ bfs-dfs/ladder_game.py
@@ -2,8 +2,15 @@
 # 뱀과 사다리 게임
 
 ladder_count, snake_count = map(int, input().split())
-map_dic = {}
 
-for i in range(ladder_count):
+# 전개도 그리기
+graph = [0] * (100)
+
+# 사다리, 뱀(이동 조건) 저장
+map_dic = {}
+for i in range(ladder_count + snake_count):
     num1, num2 = map(int, input().split())
-    map_dic[]
+    map_dic[num1] = num2
+
+
+
