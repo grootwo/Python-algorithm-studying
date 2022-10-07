db3445622c868b60b923cf66cc68cd02b332cfb9
soaringwave <himhim2514@gmail.com>
2022-10-07 23:01:43 +0900

    Update ladder_game.py
    Add little bfs function

--- bfs-dfs/ladder_game.py
+++ bfs-dfs/ladder_game.py
@@ -1,5 +1,6 @@
 # 16928
 # 뱀과 사다리 게임
+from collections import deque
 
 ladder_count, snake_count = map(int, input().split())
 
@@ -13,4 +14,13 @@ for i in range(ladder_count + snake_count):
     map_dic[num1] = num2
 
 
-
+def bfs_ladder_game():
+    queue = deque()
+    deque.append(0)
+    while queue:
+        now = queue.popleft()
+        for i in range(1, 7):
+            next = now + i
+            # 만약 다음 좌표가 인덱스 범위를 충족한다면
+            if 0 <= next < 100:
+                # 만약 사다리나 뱀이 존재한다면
