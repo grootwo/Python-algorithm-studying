e0c7f4943e4aa8dae0258601b2482c8fe25a928d
soaringwave <himhim2514@gmail.com>
2022-10-12 22:48:46 +0900

    Update print_repeat_cycle.py
    Write input part and comment

--- repeat/print_repeat_cycle.py
+++ repeat/print_repeat_cycle.py
@@ -1,7 +1,30 @@
 # 1110
 # 더하기 사이클
 
-# ㅅㅜㅅ자 받기
-# 더하기 사이클 계선'
-# 더하기 사이클 수식 완성
-# 값이 갑=ㅆ다면 사이클 출력
+# 데이터 입력 받기
+num = int(input())
+
+num_str = str(num)
+if len(num_str) == 2:
+    next = int(num_str[0]) + int(num_str[1])
+else:
+    next = num
+
+count = 1
+
+# 반복문 통해
+while next != num:
+    next_str = str(next)
+    # 한 자릿수라면
+    if len(num_str) == 2:
+        next = int(num_str[0]) + int(num_str[1])
+    # 두 자릿수라면
+    else:
+        next =
+
+# 다음 숫자 = 이전 숫자[1] + (이전 숫자[0] + 이전 숫자[1])[1]
+# 다음 숫자 만들기
+# 현재와 비교하기
+# 세기
+
+print(count)
