89462dbde297c8bfbd04bd9a1caada8e19e1b0cb
soaringwave <himhim2514@gmail.com>
2022-10-13 23:10:18 +0900

    Update print_repeat_cycle.py
    Write Code using repeat statement

--- repeat/print_repeat_cycle.py
+++ repeat/print_repeat_cycle.py
@@ -5,26 +5,28 @@
 num = int(input())
 
 num_str = str(num)
-if len(num_str) == 2:
-    next = int(num_str[0]) + int(num_str[1])
-else:
-    next = num
+if len(num_str) == 2: # 두 자릿수라면
+    num1_str = num_str[1]
+    num2_str = str(int(num_str[0]) + int(num_str[1]))
+    if len(num2_str) == 2:  # 두 자릿수라면
+        num2_str = num2_str[1]
+else:   # 한 자릿수라면
+    num1_str = num_str[0]
+    num2_str = num_str[0]
+next_str = num1_str + num2_str
 
 count = 1
 
-# 반복문 통해
-while next != num:
-    next_str = str(next)
-    # 한 자릿수라면
-    if len(num_str) == 2:
-        next = int(num_str[0]) + int(num_str[1])
-    # 두 자릿수라면
-    else:
-        next =
-
-# 다음 숫자 = 이전 숫자[1] + (이전 숫자[0] + 이전 숫자[1])[1]
-# 다음 숫자 만들기
-# 현재와 비교하기
-# 세기
+while num != int(next_str):
+    if len(next_str) == 2:  # 두 자릿수라면
+        num1_str = next_str[1]
+        num2_str = str(int(next_str[0]) + int(next_str[1]))
+        if len(num2_str) == 2:  # 두 자릿수라면
+            num2_str = num2_str[1]
+    else:  # 한 자릿수라면
+        num1_str = next_str[0]
+        num2_str = next_str[0]
+    next_str = num1_str + num2_str
+    count += 1
 
 print(count)
