6499e2e422b8585746567d3526006040b0ddb703
soaringwave <himhim2514@gmail.com>
2022-10-14 17:19:35 +0900

    Create min_max.py
    Using min, max function

---
+++ array_1/min_max.py
@@ -0,0 +1,15 @@
+# 10818
+# 최소, 최대
+
+count = int(input())
+
+nums = list(map(int, input().split()))
+
+min_num = 0
+max_num = 0
+
+min_num = min(nums)
+max_num = max(nums)
+
+print(min_num)
+print(max_num)
