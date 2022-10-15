fabdbef72c288e44d1b1db01523d15ae5c03a9ad
soaringwave <himhim2514@gmail.com>
2022-10-15 20:46:01 +0900

    Update print_ox_grade.py
    Create code counting ox grade

--- array_1/print_ox_grade.py
+++ array_1/print_ox_grade.py
@@ -4,8 +4,25 @@ import sys
 
 count = int(input())
 
-results = []
+results_ox = []
 for i in range(count):
-    results.append(sys.stdin.readline())
+    results_ox.append(sys.stdin.readline().strip())
+
 
 def count_grade(result):
+    grade = 0
+    plus = 1
+    for i in result:
+        if i == 'O':
+            grade += plus
+            plus += 1
+        else:
+            plus = 1
+    return grade
+
+resuts_grade = []
+for i in results_ox:
+    resuts_grade.append(count_grade(i))
+
+for i in resuts_grade:
+    print(i)
