53a577a554878eba64a302482891e476d5eff03d
soaringwave <himhim2514@gmail.com>
2022-10-20 23:42:15 +0900

    Update print_avg_percent.py
    Edit wrong index and string format

--- array_1/print_avg_percent.py
+++ array_1/print_avg_percent.py
@@ -4,18 +4,19 @@ import sys
 
 test_case = int(input())
 
+# 평균을 넘는 학생들의 비율들 저장
 over_avg_percent = []
+
 for i in range(test_case):
     nums = list(map(int, sys.stdin.readline().split()))
-    print(nums)
     # 평균 계산
     sum = 0
-    for j in range(1, nums[0]):
+    for j in range(1, nums[0] + 1):
         sum += nums[j]
     avg = sum / nums[0]
     # 평균 넘는 학생 수 세기
     count = 0
-    for j in range(1, nums[0]):
+    for j in range(1, nums[0] + 1):
         if nums[j] > avg:
             count += 1
     # 평균을 넘는 학생들의 비율 계산
@@ -24,4 +25,5 @@ for i in range(test_case):
     nums.clear()
 
 for i in over_avg_percent:
-    print(i)
+    # 퍼센트로 바꾸어 출력
+    print("{0:.3f}%".format(i*100))
