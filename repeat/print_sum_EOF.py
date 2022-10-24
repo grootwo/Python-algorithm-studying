4b7571be4677b4ce70a72c2f3ddeb2976af71b0b
soaringwave <himhim2514@gmail.com>
2022-10-24 23:57:32 +0900

    Create print_sum_EOF.py
    Create code using try-except statement

---
+++ repeat/print_sum_EOF.py
@@ -0,0 +1,10 @@
+# 10951
+# A+B - 4
+# 참고(https://velog.io/@jsw8050/%EB%B0%B1%EC%A4%80-while%EB%AC%B8-10951%EB%B2%88-AB-4-Python)
+
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A + B)
+    except:
+        break
