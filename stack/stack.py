d3783d62cdcab4e2b286ceb0e8ed0a9757d0345f
soaringwave <himhim2514@gmail.com>
2022-11-02 23:26:07 +0900

    Edit code
    Edit input part code and add algorithm

--- stack/stack.py
+++ stack/stack.py
@@ -1,5 +1,6 @@
 # 10828
 # 스택
+import sys
 
 stack =[]
 
@@ -11,7 +12,10 @@ def func1(x):
 
 # pop
 def func2():
- print(stack.pop())
+  if len(stack) == 0:
+    print(-1)
+  else:
+    print(stack.pop())
 
 
 # size
@@ -27,13 +31,25 @@ def func4():
     print(1)
 
 
-def top():
+# top
+def func5():
   if len(stack) != 0:
     print(stack[len(stack) - 1])
   else:
     print(-1)
 
 
+case = int(input())
 
-
-while True()
+for i in range(case):
+  answer = sys.stdin.readline().strip()
+  if answer[0:4] == 'push':
+    func1(int(answer[5:]))
+  elif answer == 'pop':
+    func2()
+  elif answer == 'size':
+    func3()
+  elif answer == 'empty':
+    func4()
+  elif answer == 'top':
+    func5()
