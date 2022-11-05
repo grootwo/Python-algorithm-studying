70e4e4f6edf64d582b5a29f1b6fb8ac840d34010
soaringwave <himhim2514@gmail.com>
2022-11-05 20:17:53 +0900

    Create cal_break_even_point.py

---
+++ basic_math1/cal_break_even_point.py
@@ -0,0 +1,16 @@
+# 1712
+# 손익분기점
+
+static_price, variable_price, cost = map(int, input().split())
+
+if variable_price >= cost:
+    print(-1)
+else:
+    total_variable_price = 0
+    total_cost = 0
+    count = 0
+    while static_price + total_variable_price >= total_cost:
+        total_variable_price += variable_price
+        total_cost += cost
+        count += 1
+    print(count)
