770315b825c1d772868e437256fc3d8a264bf1f0
soaringwave <himhim2514@gmail.com>
2022-11-06 23:56:23 +0900

    Update cal_break_even_point.py
    Chanaging calculation

--- basic_math1/cal_break_even_point.py
+++ basic_math1/cal_break_even_point.py
@@ -2,15 +2,18 @@
 # 손익분기점
 
 static_price, variable_price, cost = map(int, input().split())
+#
+# if variable_price >= cost:
+#     print(-1)
+# else:
+#     total_variable_price = 0
+#     total_cost = 0
+#     count = 0
+#     while static_price + total_variable_price >= total_cost:
+#         total_variable_price += variable_price
+#         total_cost += cost
+#         count += 1
+#     print(count)
 
-if variable_price >= cost:
-    print(-1)
-else:
-    total_variable_price = 0
-    total_cost = 0
-    count = 0
-    while static_price + total_variable_price >= total_cost:
-        total_variable_price += variable_price
-        total_cost += cost
-        count += 1
-    print(count)
+# static_price + variable_price* >= cost*
+# 이 *를 찾아야 함
