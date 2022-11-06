ac1a775fb77ac21a2b4f49e32bb508d14a79734c
soaringwave <himhim2514@gmail.com>
2022-11-07 00:18:26 +0900

    Update cal_break_even_point.py
    Edit code from repeat algorithm to calculation

--- basic_math1/cal_break_even_point.py
+++ basic_math1/cal_break_even_point.py
@@ -1,10 +1,23 @@
 # 1712
 # 손익분기점
+import math
 
 static_price, variable_price, cost = map(int, input().split())
-#
-# if variable_price >= cost:
-#     print(-1)
+
+if variable_price >= cost:
+    print(-1)
+else:
+    # 계산식: static_price + variable_price * x >= cost * x
+    tmp_BEP = static_price / (cost - variable_price)
+    # 만약 정수로 딱 떨어진다면
+    if tmp_BEP % 1 == 0.0:
+        BEP = tmp_BEP + 1
+    # 만약 소수점 아래로 수가 있다면
+    else:
+        BEP = math.ceil(tmp_BEP)
+    print(int(BEP))
+
+# 이전 반복문 코드
 # else:
 #     total_variable_price = 0
 #     total_cost = 0
@@ -14,8 +27,3 @@ static_price, variable_price, cost = map(int, input().split())
 #         total_cost += cost
 #         count += 1
 #     print(count)
-
-# static_price + variable_price* >= cost*
-# 이 *를 찾아야 함
-# static_price / (cost - variable_price) =< *
-
