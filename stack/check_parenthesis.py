432f7d8059d8b2204bd74d44495d970171357bf5
soaringwave <himhim2514@gmail.com>
2022-11-08 22:10:48 +0900

    Edit intonation

--- stack/check_parenthesis.py
+++ stack/check_parenthesis.py
@@ -5,32 +5,32 @@ stack = []
 
 
 def push(c):
-  stack.append(c)
+    stack.append(c)
 
 
 def pop():
-  stack.pop()
+    stack.pop()
 
 
 def check_VPS(string):
-  for i in string:
-    if i == '(':
-      push(i)
-    else:
-      # stack이 비어져 있는데 ')'가 입력된다면
-      if len(stack) == 0:
+    for i in string:
+        if i == '(':
+          push(i)
+        else:
+          # stack이 비어져 있는데 ')'가 입력된다면
+          if len(stack) == 0:
+            return 'NO'
+          else:
+            pop()
+    # ')'를 모두 짝지었는데 stack에 '('가 남아 있다면
+    if len(stack) != 0:
         return 'NO'
-      else:
-        pop()
-  # ')'를 모두 짝지었는데 stack에 '('가 남아 있다면
-  if len(stack) != 0:
-    return 'NO'
-  else:
-    return 'YES'
+    else:
+        return 'YES'
 
 case = int(input())
 
 for i in range(case):
-  answer = input().strip()
-  print(check_VPS(answer))
-  stack.clear()
+    answer = input().strip()
+    print(check_VPS(answer))
+    stack.clear()
--- stack/stack.py
+++ stack/stack.py
@@ -7,49 +7,49 @@ stack =[]
 
 # push
 def func1(x):
-  stack.append(x)
+    stack.append(x)
 
 
 # pop
 def func2():
-  if len(stack) == 0:
-    print(-1)
-  else:
-    print(stack.pop())
+    if len(stack) == 0:
+        print(-1)
+    else:
+        print(stack.pop())
 
 
 # size
 def func3():
-  print(len(stack))
+    print(len(stack))
 
 
 # empty
 def func4():
-  if len(stack) != 0:
-    print(0)
-  else:
-    print(1)
+    if len(stack) != 0:
+        print(0)
+    else:
+        print(1)
 
 
 # top
 def func5():
-  if len(stack) != 0:
-    print(stack[len(stack) - 1])
-  else:
-    print(-1)
+    if len(stack) != 0:
+        print(stack[len(stack) - 1])
+    else:
+        print(-1)
 
 
 case = int(input())
 
 for i in range(case):
-  answer = sys.stdin.readline().strip()
-  if answer[0:4] == 'push':
-    func1(int(answer[5:]))
-  elif answer == 'pop':
-    func2()
-  elif answer == 'size':
-    func3()
-  elif answer == 'empty':
-    func4()
-  elif answer == 'top':
-    func5()
+    answer = sys.stdin.readline().strip()
+    if answer[0:4] == 'push':
+        func1(int(answer[5:]))
+    elif answer == 'pop':
+        func2()
+    elif answer == 'size':
+        func3()
+    elif answer == 'empty':
+        func4()
+    elif answer == 'top':
+        func5()
