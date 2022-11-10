6190632f9a6507039332eb342214582b2f18a532
soaringwave <himhim2514@gmail.com>
2022-11-10 23:30:40 +0900

    Create make_list_with_stack.py

---
+++ stack/make_list_with_stack.py
@@ -0,0 +1,27 @@
+# 1874
+# 스택 수열
+
+case = int(input())
+
+nums_list = [int(input()) for _ in range(case)]
+
+stack = []
+
+def push(x):
+    stack.append(x)
+
+
+def pop():
+    return stack.pop()
+
+
+def top():
+    return stack[len(stack) - 1]
+# 자신의 순서라면 pop()
+# 할 수 있는 경우:
+#   1. 제일 마지막 원소가 해당 순서
+
+# 수열의 인덱스
+list_i = 0
+for i in range(1, case + 1):
+    
