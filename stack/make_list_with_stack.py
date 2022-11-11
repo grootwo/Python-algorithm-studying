432fb510c2ee88046c85a7b4c7c4dc49dd28b13f
soaringwave <himhim2514@gmail.com>
2022-11-11 23:56:20 +0900

    Update make_list_with_stack.py
    Add code and Edit top()

--- stack/make_list_with_stack.py
+++ stack/make_list_with_stack.py
@@ -18,7 +18,10 @@ def pop():
 
 
 def top():
-    return stack[len(stack) - 1]
+    if len(stack) != 0:
+        return stack[len(stack) - 1]
+    else:
+        return -1
 # 자신의 순서라면 pop()
 # 할 수 있는 경우:
 #   1. 제일 마지막 원소가 해당 순서
@@ -26,9 +29,15 @@ def top():
 # 수열의 인덱스
 list_i = 0
 for i in range(1, case + 1):
+    if top() == nums_list[list_i]:
+        pop()
+        list_i += 1
     if i == nums_list[list_i]:
         push(i)
         pop()
         list_i += 1
     else:
         push(i)
+
+# 수열의 인덱스가 바뀔 때마다 스택의 top, 오름차순의 순서를 확인해야 함
+# 만약 수열의 인덱스 수가 top이 아닌 스택에 있다면 그것은 불가능함
