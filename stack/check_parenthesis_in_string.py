8b1ac628fb9036b1648e360de48e6758e1045bef
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-13 23:26:08 +0900

    Update check_parenthesis_in_string.py
    Edit code

--- stack/check_parenthesis_in_string.py
+++ stack/check_parenthesis_in_string.py
@@ -14,38 +14,28 @@ def pop():
 
 
 def check_VPS_in_string(string):
-    #print(string)
     for i in string:
-        print('i:', i)
-        if i == '(' or '[':
-            print(1)
+        if i == '(' or i == '[':
             push(i)
         elif i == ')':
-            print(") rec")
-            if stack.pop() != '(':
-                #print(2)
+            if len(stack) == 0 or stack.pop() != '(':
                 return 'no'
         elif i == ']':
-            print("] rec")
-            if stack.pop() != '[':
-                print(3)
+            if len(stack) == 0 or stack.pop() != '[':
                 return 'no'
         else:
             continue
     # 모든 문자를 읽었는데 '('나 '['가 남아 있다면
     if len(stack) != 0:
-        print(4)
         return 'no'
     else:
         return 'yes'
 
 
-#print('(' == ')')
-# False인데 같게 인식함
+answer = input()
 
-answer = sys.stdin.readline().strip()
-
-while answer != '.':
+while answer != ".":
     print(check_VPS_in_string(answer))
-    answer = sys.stdin.readline().strip()
+    # 데이터, stack 초기화
+    answer = input()
     stack.clear()
