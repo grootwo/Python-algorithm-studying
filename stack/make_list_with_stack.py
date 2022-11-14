745c0282d3b4e79513b8de4330a84ff371405499
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-15 00:06:20 +0900

    Update make_list_with_stack.py
    Add command_list

--- stack/make_list_with_stack.py
+++ stack/make_list_with_stack.py
@@ -6,14 +6,15 @@ case = int(input())
 nums_list = [int(input()) for _ in range(case)]
 
 stack = []
+command_list = [] # 명령어 저장 리스트
 
 def push(x):
-    print('+')
     stack.append(x)
+    command_list.append('+')
 
 
 def pop():
-    print('-')
+    command_list.append('-')
     return stack.pop()
 
 
@@ -39,7 +40,15 @@ for i in nums_list:
             break
         push(num)
         num += 1
+        if num == case + 1:
+            command_list.append('NO')
+            break
 
+if command_list.count('NO') != 0:
+    print('NO')
+else:
+    for i in command_list:
+        print(i)
 # 수열의 인덱스가 바뀔 때마다 스택의 top, 오름차순의 순서를 확인해야 함
 # 만약 수열의 인덱스 수가 top이 아닌 스택에 있다면 그것은 불가능함
 # 수열의 인덱스가 남아있을 때까지 반복문을 돌려야 함
