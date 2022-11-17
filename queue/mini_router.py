927967af0e3677686e0efee683a94ed41a21b83a
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-17 23:14:30 +0900

    Create mini_router.py

---
+++ queue/mini_router.py
@@ -0,0 +1,24 @@
+# 15828
+# Router
+import sys
+from collections import deque
+
+# 양의 정수가 들어오면 프로세스
+# 0이 들어오면 앞에서 프로세스 하나씩 처리
+# -1은 입력의 끝
+# 마지막으로 남은 프로세스 모두 출력
+
+buffer_limit = int(input())
+
+queue = deque([])
+
+answer = int(input())
+while answer != -1:
+    if answer > 0:
+        queue.append(answer)
+    elif answer == 0:
+        queue.popleft()
+    answer = int(input())
+
+for i in queue:
+    print(i, ' ', end="")
