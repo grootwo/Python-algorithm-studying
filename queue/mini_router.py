7d2240369753fd49066f069bd08e3c511eee13f9
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-19 00:28:56 +0900

    Delete comment and edit code of output part

--- queue/mini_router.py
+++ queue/mini_router.py
@@ -3,22 +3,20 @@
 import sys
 from collections import deque
 
-# 양의 정수가 들어오면 프로세스
-# 0이 들어오면 앞에서 프로세스 하나씩 처리
-# -1은 입력의 끝
-# 마지막으로 남은 프로세스 모두 출력
-
 buffer_limit = int(input())
 
 queue = deque([])
 
 answer = int(input())
 while answer != -1:
-    if answer > 0:
+    if answer > 0 and len(queue) < buffer_limit:
         queue.append(answer)
     elif answer == 0:
         queue.popleft()
     answer = int(input())
 
-for i in queue:
-    print(i, ' ', end="")
+if len(queue) != 0:
+    for i in queue:
+        print(i, end=" ")
+else:
+    print('empty')
