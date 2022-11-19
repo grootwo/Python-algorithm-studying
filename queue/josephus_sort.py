bf44aab66106a1ac3a6ac3500a470ed7d3557abe
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-19 21:40:39 +0900

    Fix keyboard interrupt
    by Checking index

--- queue/josephus_sort.py
+++ queue/josephus_sort.py
@@ -5,6 +5,7 @@ from collections import deque
 
 person_count, josephus_num = map(int, input().split())
 
+# queue에 오름차순 순서대로 저장
 queue = deque()
 for i in range(1, person_count + 1):
     queue.append(i)
@@ -12,8 +13,14 @@ for i in range(1, person_count + 1):
 print('<', end="")
 while len(queue) >= 1:
     for i in range(josephus_num):
-        if i != josephus_num:
-            queue.append(queue.popleft())
+        if i != josephus_num - 1:
+            num = queue.popleft()
+            queue.append(num)
+        # 조세푸스 수에 해당되면 출력하기
         else:
-            print(queue.popleft(), end=", ")
-print('>', end="")
+            if len(queue) != 1:
+                print(queue.popleft(), end=", ")
+            # 마지막 수는 반점 출력하지 않기
+            else:
+                print(queue.popleft(), end="")
+print('>')
