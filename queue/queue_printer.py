4268d01e3afc8966a10fbd52e9bfcee613c35b16
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-20 21:29:52 +0900

    Edit break statement
    By making function printing count

--- queue/queue_printer.py
+++ queue/queue_printer.py
@@ -14,21 +14,24 @@ def check_max_importance(lists):
     return max_importance
 
 
+def print_count(queue):
+    count = 0
+    while queue:
+        if queue[0][1] == check_max_importance(queue):
+            count += 1
+            temp = queue.popleft()
+            if temp[0] == curious_num:
+                print(count)
+                return
+        else:
+            temp = queue.popleft()
+            queue.append(temp)
+
+
 for i in range(case):
     document_count, curious_num = map(int, input().split())
     queue = deque([])
     importances = list(map(int, input().split()))
     for j in range(document_count):
         queue.append([j, importances[j]])
-    count = 0
-    while queue:
-        if queue[0][1] == check_max_importance(queue):
-            count += 1
-            if queue.popleft() == curious_num:
-                break
-        else:
-            temp = queue.popleft()
-            queue.append(temp)
-    print(count)
-
-
+    print_count(queue)
