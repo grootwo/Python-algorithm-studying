4180c38c589cd66c37d875402b874d7e1cd6640e
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-23 17:40:43 +0900

    Create AC.py

---
+++ queue/AC.py
@@ -0,0 +1,11 @@
+# 5430
+# AC
+
+case = int(input())
+
+for i in range(case):
+    funcs = input()
+    nums_count = int(input())
+    nums_list_input = input()
+    # 이거 [1, 2, 3, 4] 이런 입력 들어오면 어떻게 할 건지 찾아봐라
+    nums_list = nums_list_input[]
