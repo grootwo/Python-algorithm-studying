6ced4a120525ebe5eadb16ca0716340bae3c3637
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-11-28 08:46:06 +0900

    Edit comment

--- queue/AC.py
+++ queue/AC.py
@@ -7,15 +7,22 @@ case = int(input())
 
 
 def R(array):
-    # editting
+    array.reverse()
+    return array
+
 
 def D(array):
-    # editing
+    if array:
+        array.popleft()
+        return array
+    else:
+        return False
 
 
 for i in range(case):
     funcs = input()
     nums_count = int(input())
+    # 리스트 입력받기
     if nums_count == 0:
         nums_list_input = input()
         nums_list = []
@@ -23,14 +30,16 @@ for i in range(case):
         nums_list_input = input()
         nums_list = list(map(int, nums_list_input[1:-1].split(',')))
     nums_queue = deque(nums_list)
+    # 명령 실행하기
     for j in funcs:
         if j == 'R':
             nums_queue = R(nums_queue)
         else:
             nums_queue = D(nums_queue)
-            if nums_queue == False:
+            if nums_queue is False:
                 break
-    if nums_queue != False:
+    # 결과 출력하기
+    if nums_queue is not False:
         print('[', end='')
         while nums_queue:
             if len(nums_queue) > 1:
@@ -38,8 +47,6 @@ for i in range(case):
                 print(',', end='')
             else:
                 print(nums_queue.popleft(), end='')
-        #for j in range(len(nums_queue)):
-        #    print(nums_queue[j], end='')
-        #    if j != len(nums_queue) - 1:
-        #        print(',', end='')
         print(']')
+    else:
+        print('error')
