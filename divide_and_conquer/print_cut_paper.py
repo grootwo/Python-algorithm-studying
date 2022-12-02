6cccf9f299c11a8b43d95ed6b2fcc0677dbab949
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-03 00:25:41 +0900

    Add list slicing code

--- divide_and_conquer/print_cut_paper.py
+++ divide_and_conquer/print_cut_paper.py
@@ -1,6 +1,7 @@
 # 2630
 # 색종이 만들기
 import sys
+from collections import deque
 
 n = int(input())
 
@@ -22,6 +23,30 @@ def check_same(graph):
 # 그 리스트를 확인한다
 # 만약 모두 같지 않다면 분할하여 큐에 넣는다
 # 만약 리스트의 원소가 하나라면 최소이니 더 이상 분할하지 않는다
-
+count = 0
 que = deque([])
-
+que.append(graph)
+while que:
+    check_list = que.popleft()
+    if check_same(que) is True:
+        count += 1
+    else:
+        length = len(check_list)
+        if length != 1:
+            for i in range(4):
+                # 2사분면
+                temp_list1 = check_list[0:length/2]
+                for j in range(length/2):
+                    temp_list1[j][0:length/2]
+                # 3사분면
+                temp_list2 = check_list[length/2:length]
+                for j in range(length/2):
+                    temp_list2[j][0:length/2]
+                # 1사분면
+                temp_list3 = check_list[0:length/2]
+                for j in range(length/2):
+                    temp_list2[j][length/2:length]
+                # 4사분면
+                temp_list3 = check_list[length/2:length]
+                for j in range(length/2):
+                    temp_list2[j][length/2:length]
