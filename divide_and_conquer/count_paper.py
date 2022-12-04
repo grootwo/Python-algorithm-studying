644cfea600bb7cfce9e8826f69692596169816ba
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-04 23:59:19 +0900

    Add checking func and input part

--- divide_and_conquer/count_paper.py
+++ divide_and_conquer/count_paper.py
@@ -1,2 +1,18 @@
 # 1780
 # 색종이의 개수
+import sys
+
+
+def check_num(graph):
+    num = graph[0][0]
+    for i in range(len(graph)):
+        for j in range(len(graph[0])):
+            if graph[i][j] != num:
+                return False
+
+
+n = int(input())
+graph = []
+for i in range(n):
+    graph.append(map(int, sys.stdin.readline().split()))
+print(graph)
