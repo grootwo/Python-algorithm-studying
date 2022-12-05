977e0268d1760d37c53a95fd0d04c7606670dc4f
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-05 09:45:27 +0900

    Delete checking func and add counting &cutting func

--- divide_and_conquer/count_paper.py
+++ divide_and_conquer/count_paper.py
@@ -2,17 +2,39 @@
 # 색종이의 개수
 import sys
 
-
-def check_num(graph):
-    num = graph[0][0]
-    for i in range(len(graph)):
-        for j in range(len(graph[0])):
-            if graph[i][j] != num:
-                return False
-
-
 n = int(input())
-graph = []
-for i in range(n):
-    graph.append(map(int, sys.stdin.readline().split()))
-print(graph)
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+count_minus_1 = 0
+count_0 = 0
+count_1 = 0
+
+
+def count_or_cut_paper(x, y, n):
+    global count_minus_1, count_0, count_1
+    num = graph[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if num != graph[i][j]:
+                count_or_cut_paper(x, y, n//3)
+                count_or_cut_paper(x, y+n//3, n//3)
+                count_or_cut_paper(x, y+n//3*2, n//3)
+                count_or_cut_paper(x+n//3, y, n//3)
+                count_or_cut_paper(x+n//3, y+n//3, n//3)
+                count_or_cut_paper(x+n//3, y+n//3*2, n//3)
+                count_or_cut_paper(x+n//3*2, y, n//3)
+                count_or_cut_paper(x+n//3*2, y+n//3, n//3)
+                count_or_cut_paper(x+n//3*2, y+n//3*2, n//3)
+                return
+    if num == -1:
+        count_minus_1 += 1
+    elif num == 0:
+        count_0 += 1
+    else:
+        count_1 += 1
+
+
+count_or_cut_paper(0, 0, n)
+print(count_minus_1)
+print(count_0)
+print(count_1)
