4bd9fed8750b928ca3b454a97452120b29a335bc
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-06 22:36:57 +0900

    Create multiply_matrix.py

---
+++ divide_and_conquer/multiply_matrix.py
@@ -0,0 +1,13 @@
+# 2740
+# 행렬곱셈
+import sys
+
+row_a, col_a = map(int, input().split())
+mat_a = [list(map(int, sys.stdin.readline().split()))]
+
+row_b, col_b = map(int, input().split())
+mat_b = [list(map(int, sys.stdin.readline().split()))]
+
+print(mat_a)
+print('-------------')
+print(mat_b)
