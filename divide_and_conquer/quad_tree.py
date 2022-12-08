2360d18de0be5eecd41f26b2a80cd6b8d65a5d7b
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-08 23:30:02 +0900

    Add comment

--- divide_and_conquer/quad_tree.py
+++ divide_and_conquer/quad_tree.py
@@ -10,7 +10,9 @@ def print_or_cut(x, y, n):
     num = graph[x][y]
     for i in range(x, x+n):
         for j in range(y, y+n):
+            # 이차원 리스트 안의 원소가 하나라도 다르다면
             if num != graph[i][j]:
+                # 괄호 출력, 이차원 리스트 쪼개기
                 print('(', end='')
                 print(print_or_cut(x, y, n//2), end='')
                 print(print_or_cut(x, y+n//2, n//2), end='')
@@ -18,6 +20,7 @@ def print_or_cut(x, y, n):
                 print(print_or_cut(x+n//2, y+n//2, n//2), end='')
                 print(')', end='')
                 return ''
+    # 이차원 리스트 안의 원소가 모두 같다면 해당 원소 출력
     return num
 
 
