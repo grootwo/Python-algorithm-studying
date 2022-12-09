420b96fe03073c6c00211e0fe8f9c5057a0ec74a
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-09 21:55:08 +0900

    Create mat_multiply.py

---
+++ divide_and_conquer/mat_multiply.py
@@ -0,0 +1,10 @@
+# 2740
+# 행렬곱셈
+import sys
+
+row_a, col_a = map(int, input().split())
+mat_a = [list(map(int, sys.stdin.readline().split())) for _ in range(row_a)]
+
+row_b, col_b = map(int, input().split())
+mat_b = [list(map(int, sys.stdin.readline().split())) for _ in range(row_b)]
+
