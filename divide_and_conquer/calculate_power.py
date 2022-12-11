1f2e871b1ed07fcecd4458795d51e11caa097ebf
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-11 21:44:21 +0900

    Write another code with repeat statement

--- divide_and_conquer/calculate_power.py
+++ divide_and_conquer/calculate_power.py
@@ -2,14 +2,14 @@
 # 곱셈
 import sys
 from collections import deque
+from math import floor
 
 num, power_num, divider = map(int, sys.stdin.readline().split())
 que = deque([num for _ in range(power_num)])
 
+
 while len(que) != 1:
-    num = que.pop()
-    num = que.pop() * num
-    que.appendleft(num)
-    print(que)
+    for i in range(floor(len(que) / 2)):
+        que[i] *= que.pop()
 
 print(que.popleft() % divider)
