c2769e0055297fa2809f25cc8c74f6652bceb241
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-12 21:20:31 +0900

    Delete importing code

--- divide_and_conquer/calculate_power.py
+++ divide_and_conquer/calculate_power.py
@@ -1,8 +1,6 @@
 # 1629
 # 곱셈
 import sys
-from collections import deque
-from math import floor
 
 num, power_num, divider = map(int, sys.stdin.readline().split())
 que = deque([num for _ in range(power_num)])
