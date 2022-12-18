c60ec47ce5541ef7a339b77aecf8be6dabc86fc5
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-18 14:29:33 +0900

    Update code getting mode

--- sort/print_four_result.py
+++ sort/print_four_result.py
@@ -22,13 +22,17 @@ def get_median(nums):
 def get_mode(nums):
     max_count = 0
     check = []
-    for i in range(len(nums)):
-        if nums[i] not in check and nums.count(i) >= max_count:
-            max_count = nums.count(i)
-            check.append(nums[i])
-        else:
-            continue
-
+    i = 0
+    while i < len(nums):
+        num = nums[i]
+        count = nums.count(num)
+        check.append([num, count]) # 수와 해당 수의 개수를 저장
+        i += count # 셈이 중복되지 않도록
+    check.sort(key=lambda x:(-x[1], x[0]))
+    if len(check) >= 2 and check[0][1] == check[1][1]: # 만약 원소가 2개 이상이고, 빈도수가 같은 원소가 있다면
+        return check[1][0] # 두 번째로 작은 수를 리턴
+    else:
+        return check[0][0]
 
 
 def get_scope(nums):
@@ -37,5 +41,5 @@ def get_scope(nums):
 
 print(get_mean(nums))
 print(get_median(nums))
-# print(get_mode())
+print(get_mode(nums))
 print(get_scope(nums))
