d3a2c20787f7911d7246bdfc4ed69cebc3bdaeeb
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-20 23:34:10 +0900

    Create sort_with_repeat_num.py

---
+++ sort/sort_with_repeat_num.py
@@ -0,0 +1,15 @@
+# 10989
+# 수 정렬하기 3
+import sys
+
+n = int(sys.stdin.readline())
+
+nums = [0] * 10001
+
+for _ in range(n):
+    nums[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if nums[i] != 0:
+        for _ in range(nums[i]):
+            print(i)
