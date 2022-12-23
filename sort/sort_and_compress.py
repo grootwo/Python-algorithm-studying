903504ab57883d72398d64cf825ee4f74003553e
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-23 23:19:11 +0900

    Making code with Counter

--- sort/sort_and_compress.py
+++ sort/sort_and_compress.py
@@ -1,16 +1,22 @@
 # 18870
 # 좌표 압축
 import sys
+from collections import Counter
 
 n = int(sys.stdin.readline())
 
 nums = list(map(int, sys.stdin.readline().split()))
 
-nums_sort = []
-for i in range(n):
-    if nums[i] not in nums_sort:
-        nums_sort.append(nums[i])
-nums_sort.sort()
+nums_counter = Counter(nums)
+print(nums_counter)
+nums_sort = [0] * len(nums_counter)
+print(nums_counter)
+for i in range(len(nums_counter)):
+    index = nums.index(nums_counter[i][0])
+    print('i', index)
+    nums_sort[index] = nums_counter[i][1]
+    print(nums_sort)
+    print('------')
 
 for i in range(n):
     index = nums_sort.index(nums[i])
