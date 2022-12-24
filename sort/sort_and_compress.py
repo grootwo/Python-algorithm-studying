2bdba06f743a28e85bd4f81395392a9fbbc40862
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-24 19:07:06 +0900

    Using dic and limit range

--- sort/sort_and_compress.py
+++ sort/sort_and_compress.py
@@ -1,7 +1,6 @@
 # 18870
 # 좌표 압축
 import sys
-from collections import Counter
 
 n = int(sys.stdin.readline())
 
@@ -9,8 +8,8 @@ nums = list(map(int, sys.stdin.readline().split()))
 
 nums_sort = sorted(list(set(nums)))
 dic = {}
-for i in range(n):
-    dic[nums[i]] = nums_sort.index(nums[i])
+for i in range(len(nums_sort)):
+    dic[nums_sort[i]] = i
 
 for i in range(n):
     print(dic[nums[i]], end=' ')
