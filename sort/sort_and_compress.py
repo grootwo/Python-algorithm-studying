d121b8d091b58180ef2cb235e9b6e757335e9205
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-24 18:57:28 +0900

    Using dic

--- sort/sort_and_compress.py
+++ sort/sort_and_compress.py
@@ -7,19 +7,10 @@ n = int(sys.stdin.readline())
 
 nums = list(map(int, sys.stdin.readline().split()))
 
-nums_counter = Counter(nums)
-nums_sort = list(nums_counter.keys())
-nums_sort.sort()
-# for i in range(len(nums_counter)):
-#     index = nums.index(nums_counter[i][0])
-#     print('i', index)
-#     nums_sort[index] = nums_counter[i][1]
-#     print(nums_sort)
-#     print('------')
+nums_sort = sorted(list(set(nums)))
+dic = {}
+for i in range(n):
+    dic[nums[i]] = nums_sort.index(nums[i])
 
 for i in range(n):
-    index = nums_sort.index(nums[i])
-    if i != len(nums) - 1:
-        print(index, end=' ')
-    else:
-        print(index)
+    print(dic[nums[i]], end=' ')
