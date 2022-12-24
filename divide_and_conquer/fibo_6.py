fa407004d2adbd063caa5fb62c9769f4f0ca43d5
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-24 16:31:38 +0900

    Edit limit and Add divider

--- divide_and_conquer/fibo_6.py
+++ divide_and_conquer/fibo_6.py
@@ -1,15 +1,15 @@
 # 11444
 # 피보나치 수 6
 import sys
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 9)
 
 n = int(sys.stdin.readline())
 
 def fibo(num):
     if num == 0:
-        return 0
+        return 0 % 1000000007
     elif num == 1:
-        return 1
+        return 1 % 1000000007
     else:
         return (fibo(num - 1) + fibo(num - 2)) % 1000000007
 
