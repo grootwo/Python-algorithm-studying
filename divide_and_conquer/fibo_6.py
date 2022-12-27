d091730b4aa4e0a699d5557080d04dd06b6e4eba
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-27 22:53:57 +0900

    Make fibo func using deque

--- divide_and_conquer/fibo_6.py
+++ divide_and_conquer/fibo_6.py
@@ -1,22 +1,31 @@
 # 11444
 # 피보나치 수 6
 import sys
+from collections import deque
+
 sys.setrecursionlimit(10 ** 9)
 
 n = int(sys.stdin.readline())
 
 # def fibo(num):
 #     if num == 0:
-#         return 0 % 1000000007
+#         return 0
 #     elif num == 1:
-#         return 1 % 1000000007
+#         return 1
 #     else:
 #         return (fibo(num - 1) + fibo(num - 2)) % 1000000007
-#
-# print(fibo(n))
 
-fibo = [0] * (n + 1)
-fibo[1] = 1
-for i in range(2, n + 1):
-    fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1000000007
-print(fibo[n])
+def fibo(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        que = deque([0, 1])
+        for _ in range(num - 1):
+            result = que[0] + que[1]
+            que.popleft()
+            que.append(result)
+        return que[1] % 1000000007
+
+print(fibo(n))
