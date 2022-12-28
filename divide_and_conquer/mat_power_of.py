446c4bfbfb724689f9033ba9ca19338f69a8f2fe
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-28 14:42:02 +0900

    Edit code by referencing other's code

--- divide_and_conquer/mat_power_of.py
+++ divide_and_conquer/mat_power_of.py
@@ -1,35 +1,34 @@
 # 10830
 # 행렬 제곱
+# ref(https://junior-datalist.tistory.com/223)
 import sys
 
 n, power = map(int, sys.stdin.readline().split())
 mat = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
-mat_temp = [[0] * n for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        mat_temp[i][j] = mat[i][j]
-mat_result = [[0] * n for _ in range(n)]
-
-for l in range(power - 1):
-    # 곱셈 한 번 실행
+# 행렬 곱셈
+def mat_multiply(mat_a, mat_b):
+    mat_result = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
-            mat_result[i][j] = 0
             for k in range(n):
-                mat_result[i][j] += mat[i][k] * mat_temp[k][j]
-            mat_result[i][j] = mat_result[i][j] % 1000
-    # print(l, '-----------')
-    # for i in range(n):
-    #     print(*mat_result[i])
-    # temp에 result값 옮겨주기
-    for i in range(n):
-        for j in range(n):
-            mat_temp[i][j] = mat_result[i][j]
-    # for i in range(n):
-    #     print(*mat_temp[i])
-    # for i in range(n):
-    #     print(*mat[i])
+                mat_result[i][j] += mat_a[i][k] * mat_b[k][j]
+            mat_result[i][j] %= 1000
+    return mat_result
+
+
+def divdie_power(mat, power):
+    if power == 1:
+        # 여기에 바로 1000 나눈 나머지 넣어도 되는지 고민
+        return mat
+    elif power % 2 == 0:
+        temp = divdie_power(mat, power//2)
+        return mat_multiply(temp, temp)
+    elif power % 2 == 1:
+        temp = divdie_power(mat, n - 1)
+        return mat_multiply(temp, temp)
+
+result = divdie_power(mat, power)
 
 for i in range(n):
-    print(*mat_result[i])
+    print(*result[i])
