ba9e5917a7b288a8e960d49ed89bd43085075b5c
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-28 22:10:18 +0900

    Switch n to right variable

--- divide_and_conquer/mat_power_of.py
+++ divide_and_conquer/mat_power_of.py
@@ -8,25 +8,36 @@ mat = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 
 # 행렬 곱셈
 def mat_multiply(mat_a, mat_b):
+    # print('func mul')
     mat_result = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 mat_result[i][j] += mat_a[i][k] * mat_b[k][j]
             mat_result[i][j] %= 1000
+    # for i in range(n):
+    #     print(*mat_result[i])
     return mat_result
 
-
+# 제곱 분할
 def divdie_power(mat, power):
+    # print('func pow')
     if power == 1:
-        # 여기에 바로 1000 나눈 나머지 넣어도 되는지 고민
+        # print('power = 1')
+        for i in range(n):
+            for j in range(n):
+                mat[i][j] %= 1000
         return mat
     elif power % 2 == 0:
+        # print('power % 2 == 0')
+        # print('power', power)
         temp = divdie_power(mat, power//2)
         return mat_multiply(temp, temp)
-    elif power % 2 == 1:
-        temp = divdie_power(mat, n - 1)
-        return mat_multiply(temp, temp)
+    else:
+        # print('power % 2 = 1')
+        # print('power', power)
+        temp = divdie_power(mat, power - 1)
+        return mat_multiply(temp, mat)
 
 result = divdie_power(mat, power)
 
