4b7a256d5d041896a570a32d44c28b475684dd46
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-30 21:48:15 +0900

    Make binary search func

--- binary_search/find_num.py
+++ binary_search/find_num.py
@@ -6,9 +6,27 @@ n = int(sys.stdin.readline())
 list_n = list(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 list_m = list(map(int, sys.stdin.readline().split()))
+list_n.sort()
+
+
+def binary_search(start, end, num):
+    if start == end:
+        if num == list_n[start]:
+            print(1)
+        else:
+            print(0)
+    elif start > end:
+        print(0)
+    else:
+        mid = (end + start) // 2
+        if num == list_n[mid]:
+            print(1)
+            return 1
+        elif num > list_n[mid]:
+            binary_search(mid + 1, end, num)
+        elif num < list_n[mid]:
+            binary_search(start, mid - 1, num)
+
 
 for i in list_m:
-    if i in list_n:
-        print(1)
-    else:
-        print(0)
+    binary_search(0, len(list_n) - 1, i)
