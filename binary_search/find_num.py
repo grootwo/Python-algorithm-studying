446ce6949de94496a1130bd37aafc14ff74b9266
leegaeun <82134672+soaringwave@users.noreply.github.com>
2022-12-31 19:35:38 +0900

    Edit return statement

--- binary_search/find_num.py
+++ binary_search/find_num.py
@@ -13,15 +13,17 @@ def binary_search(start, end, num):
     if start == end:
         if num == list_n[start]:
             print(1)
+            return
         else:
             print(0)
+            return
     elif start > end:
         print(0)
     else:
         mid = (end + start) // 2
         if num == list_n[mid]:
             print(1)
-            return 1
+            return
         elif num > list_n[mid]:
             binary_search(mid + 1, end, num)
         elif num < list_n[mid]:
