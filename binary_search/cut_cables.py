4faa0424bfdfa2d0714d433c574535ff0c29175b
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-25 23:13:45 +0900

    Edit code with ref

--- binary_search/cut_cables.py
+++ binary_search/cut_cables.py
@@ -1,60 +1,21 @@
 # 1654
 # 랜선 자르기
+# 참고 (https://claude-u.tistory.com/443)
 import sys
-sys.setrecursionlimit(10 ** 6)
 
-k, n = map(int, sys.stdin.readline().split())
-cables = [int(sys.stdin.readline()) for _ in range(k)]
+cable_count, need = map(int, input().split())
+cables = [int(sys.stdin.readline()) for _ in range(cable_count)]
+start, end = 1, max(cables)
 
-# 가장 작은 케이블을 선택한다
-min_cable = cables[0]
-for i in range(1, len(cables)):
-    if min_cable > cables[i]:
-        min_cable = cables[i]
-
-
-# 이분 탐색으로 적절한 cm를 찾는다
-def find_max_cable():
-    # print("--------")
-    # print("find")
-    # 첫 가능
-    temp = binary_search(1, min_cable)
-    # print("temp:", temp)
-    for i in range(temp + 1, min_cable):
-        if is_adequate_cable(i) == -1:
-            return i - 1
-
-
-# cm가 조건을 만족하면서 최대인지 확인하는 함수
-def is_adequate_cable(cm):
-    # print("--------")
-    # print("adequate")
-    global n
+while start <= end:
+    mid = (start + end) // 2
     count = 0
     for i in cables:
-        count += i // cm
-    if count == n:
-        # print("0")
-        return 0
-    elif count > n: # 케이블이 최대보다 짧다면
-        # print("1")
-        return 1
-    elif count < n: # 케이블이 최대보다 길다면
-        # print("-1")
-        return -1
+        count += i // mid
 
+    if count >= need:
+        start = mid + 1
+    else:
+        end = mid - 1
 
-def binary_search(start, end):
-    # print("--------")
-    # print("binary")
-    mid = (end + start) // 2
-    if is_adequate_cable(mid) == 0:
-        # print("mid:", mid)
-        return mid
-    elif is_adequate_cable(mid) == 1:
-        return binary_search(mid + 1, end)
-    elif is_adequate_cable(mid) == -1:
-        return binary_search(start, mid - 1)
-
-
-print(find_max_cable())
+print(end)
