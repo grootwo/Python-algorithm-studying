66c6dd37171e8180b9a238bb1403cc6a76225545
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-30 22:02:26 +0900

    Write code using binary search

--- binary_search/cut_trees.py
+++ binary_search/cut_trees.py
@@ -2,6 +2,29 @@
 # 나무 자르기
 import sys
 
-count, need = map(int, sys.stdin.readline().split())
+tree_count, need = map(int, sys.stdin.readline().split())
 trees = list(map(int, sys.stdin.readline().split()))
 
+start = 1
+end = max(trees)
+
+
+def availableTree(num):
+    count = 0
+    for i in trees:
+        if i > num:
+            count += (i - num)
+    if count >= need:
+        return 1
+    else:
+        return 0
+
+while start <= end:
+    mid = (start + end) // 2
+
+    if availableTree(mid):
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
