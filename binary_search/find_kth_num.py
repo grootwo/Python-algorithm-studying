2dbce0934405cb3ec58a080617c0585a78c72140
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-14 16:55:53 +0900

    Update find_kth_num.py

--- binary_search/find_kth_num.py
+++ binary_search/find_kth_num.py
@@ -1,24 +1,24 @@
 # 1300
 # K번째 수
+# ref(https://hongcoding.tistory.com/13)
 
 n = int(input())
 find_index = int(input())
 
-array_a = [[0] * n for _ in range(n)]
-array_b = []
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        num = i * j
-        array_a[i - 1][j - 1] = num
-        array_b.append(num)
-array_b.sort()
+def binary_search(target, start, end):
+    while(start <= end):
+        mid = (start + end) // 2
 
-start = 0
-end = n * n - 1
-while start <= end:
-    mid = (start + end) // 2
+        cnt = 0
+        for i in range(1, n + 1):
+            cnt += min(mid // i, n)
 
-    if mid == find_index:
-        # 왜 인덱스를 활용하지 않고 이분탐색을 이용하는 거지
-    # 왜 행렬을 사용하는 거여?
+        if cnt >= target:
+            end = mid-1
+        else:
+            start = mid+1
+    return start
+
+
+print(binary_search(find_index, 1, n * n))
