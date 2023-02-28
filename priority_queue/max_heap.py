69a2e3eba2bcc1b7b4397b88c17beb3ad58379aa
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-02-28 21:42:20 +0900

    Update max_heap.py

--- priority_queue/max_heap.py
+++ priority_queue/max_heap.py
@@ -5,10 +5,18 @@
 # 여러 값들 중에 최댓값이나 최솟값을 빠르게 찾아낼 수 있다고 한다
 
 # 최대 힙: 부모 노드의 키 값이 자식 노드의 키 값보다 크거나 같은 완전 이진 트리
-
-
+import sys
+import heapq as hq
 
 cal_count = int(input())
 
+heap = []
 for i in range(cal_count):
-    command = int(input())
+    command = int(sys.stdin.readline())
+    if command:
+        hq.heappush(heap, (-command, command))
+    else:
+        if heap:
+            print(hq.heappop(heap)[1])
+        else:
+            print(0)
