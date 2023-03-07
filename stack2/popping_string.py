a1a2a79e3fb0a56190c0607a7b86a97ae23b5d07
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-07 12:47:16 +0900

    Fix by appending

--- stack2/popping_string.py
+++ stack2/popping_string.py
@@ -10,19 +10,23 @@ last_ch = popping_string[-1]
 que = deque([])
 
 for i in range(len(int_list)):
-    if int_list[i] == last_ch and len(que) >= len(popping_string) - 1: # 폭발 문자의 마지막 글자와 같고, 앞의 인덱스를 확일할 정도의 길이가 된다면
-        # 확인하기
+    if int_list[i] == last_ch and len(que) >= len(popping_string) - 1: # 폭발 문자의 마지막 글자와 같고, 앞의 글자들까지 확인할 정도의 길이가 된다면
+        # 폭발 문자와 같은지 확인하기
         check = False
         for j in range(1, len(popping_string)):
             if que[len(que) - j] != popping_string[len(popping_string) - 1 - j]:
                 break
         else:
             check = True
-        if check is True:
+        # print("check:", check)
+        if check is True: # 폭발 문자와 같다면 삭제
             for j in range(len(popping_string) - 1):
                 que.pop()
+        else: # 폭발 문자가 아니라면 추가
+            que.append(int_list[i])
     else:
         que.append(int_list[i])
+    # print(que)
 
 if que:
     print("".join(que))
