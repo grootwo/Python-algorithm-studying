65673ad643d858384c8c4adbdb7127d2a33ddf36
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-07 22:34:31 +0900

    Make with stack O(n)

--- stack2/print_NEG.py
+++ stack2/print_NEG.py
@@ -1,17 +1,20 @@
 # 17298
 # 오큰수
 
+# 시간 복잡도 O(n) 사용해야 한다
+
 import sys
 
-count = int(input())
+count = int(sys.stdin.readline())
 int_list = list(map(int, sys.stdin.readline().split()))
 
-for i in range(len(int_list)):
-    check = False
-    for j in range(i + 1, len(int_list)):
-        if int_list[i] < int_list[j]:
-            print(int_list[j], end=" ")
-            check = True
-            break
-    if check is False:
-        print(-1, end=" ")
+answer = [-1] * count
+stack = []
+
+for i in range(count):
+    while stack and stack[-1][0] < int_list[i]:
+        temp, index = stack.pop()
+        answer[index] = int_list[i]
+    stack.append([int_list[i], i])
+
+print(*answer)
