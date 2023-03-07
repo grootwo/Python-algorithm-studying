b77332c67a61882de08c26e80a4721e775dad43e
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-07 10:20:44 +0900

    Make with for statement

--- stack2/print_NEG.py
+++ stack2/print_NEG.py
@@ -1,3 +1,17 @@
 # 17298
 # 오큰수
 
+import sys
+
+count = int(input())
+int_list = list(map(int, sys.stdin.readline().split()))
+
+for i in range(len(int_list)):
+    check = False
+    for j in range(i + 1, len(int_list)):
+        if int_list[i] < int_list[j]:
+            print(int_list[j], end=" ")
+            check = True
+            break
+    if check is False:
+        print(-1, end=" ")
