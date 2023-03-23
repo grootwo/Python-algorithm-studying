936de2c713a04b00800fd607c1264e8fde13631d
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-23 12:53:29 +0900

    Change variable name and delete unnecessary list

--- stack2/popping_string.py
+++ stack2/popping_string.py
@@ -2,15 +2,14 @@
 # 문자열 폭발
 from collections import deque
 
-strings = input()
-int_list = list(strings)
+string = input()
 popping_string = list(input())
 last_ch = popping_string[-1]
 
 que = deque([])
 
-for i in range(len(int_list)):
-    if int_list[i] == last_ch and len(que) >= len(popping_string) - 1: # 폭발 문자의 마지막 글자와 같고, 앞의 글자들까지 확인할 정도의 길이가 된다면
+for i in range(len(string)):
+    if string[i] == last_ch and len(que) >= len(popping_string) - 1: # 폭발 문자의 마지막 글자와 같고, 앞의 글자들까지 확인할 정도의 길이가 된다면
         # 폭발 문자와 같은지 확인하기
         check = False
         for j in range(1, len(popping_string)):
@@ -22,9 +21,9 @@ for i in range(len(int_list)):
             for j in range(len(popping_string) - 1):
                 que.pop()
         else: # 폭발 문자가 아니라면 추가
-            que.append(int_list[i])
+            que.append(string[i])
     else:
-        que.append(int_list[i])
+        que.append(string[i])
 
 if que:
     print("".join(que))
