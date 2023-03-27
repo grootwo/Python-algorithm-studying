25f2036eeae4c2b3bdd71c2ae7e47edf934542bd
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-27 22:54:01 +0900

    Use checking system

--- unorganized_file/binomial_coefficient.py
+++ unorganized_file/binomial_coefficient.py
@@ -5,12 +5,20 @@ import sys
 sys.setrecursionlimit(10**6)
 
 n, k = map(int, input().split())
+check = 0
+
 
 def factorial(num):
+    global check
+    print("num and check", num, check)
+    if check >= k:
+        return num
+    check += 1
     if num == 1:
         return 1
     else:
         return factorial(num - 1) * num
+
 
 result = int(factorial(n) / factorial(k)) % 1000000007
 print(result)
