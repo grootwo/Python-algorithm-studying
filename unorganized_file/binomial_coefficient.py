02c5ac7e3a6dfdaff8cd4030690025a041fa2ad3
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-03-27 22:49:29 +0900

    Make factorial function

--- unorganized_file/binomial_coefficient.py
+++ unorganized_file/binomial_coefficient.py
@@ -1,6 +1,16 @@
 # 11401
 # 이항 계수 3
+import sys
+
+sys.setrecursionlimit(10**6)
 
 n, k = map(int, input().split())
 
-print(n, k)
+def factorial(num):
+    if num == 1:
+        return 1
+    else:
+        return factorial(num - 1) * num
+
+result = int(factorial(n) / factorial(k)) % 1000000007
+print(result)
