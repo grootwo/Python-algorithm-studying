5b82f1c604371ff42833386762b604d357ed2c5f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-04-28 13:04:10 +0900

    Change the way to find X

--- unorganized_file/walking_dog_in_park.py
+++ unorganized_file/walking_dog_in_park.py
@@ -6,60 +6,41 @@ def solution(park, routes):
     for i in range(row):
         for j in range(col):
             if park[i][j] == "S":
-                start_r = i
-                start_c = j
+                now_r = i
+                now_c = j
     # move per command
-    now_r = start_r
-    now_c = start_c
+    ahead_i = [[0, 1], [0, -1], [1, 0], [-1, 0]]
     for i in range(len(routes)):
+        print("-------------------")
+        next_r = now_r
+        next_c = now_c
         ahead = routes[i][0]
         block = int(routes[i][2])
-        # row = 0 col = 1
-        change_check = 0
+        check = 0
         if ahead == "E":
-            next_c = now_c + block
-            next_r = now_r 
-            change_check = 1
+            go = ahead_i[0]
         elif ahead == "W":
-            next_c = now_c - block
-            next_r = now_r
-            change_check = 1
+            go = ahead_i[1]
         elif ahead == "S":
-            next_c = now_c
-            next_r = now_r + block
-            change_check = 0
+            go = ahead_i[2]
         elif ahead == "N":
-            next_c = now_c
-            next_r = now_r - block
-            change_check = 0
-        print("-------------------")
-        print("now: [", now_r, ",", now_c, "]")
-        print("next: [", next_r, ",", next_c, "]")
-        # check range
-        if next_r < 0 or row <= next_r or next_c < 0 or col <= next_c:
-            print("out of range")
+            go = ahead_i[3]
+        for j in range(block):
+            next_r += go[0]
+            next_c += go[1]
+            # check
+            print("now: ", now_r, now_c)
+            print("next: ", next_r, next_c)
+            if next_r < 0 or row <= next_r or next_c < 0 or col <= next_c or park[next_r][next_c] == "X":
+                print("can't go")
+                check = 1
+                break
+                
+        if check == 1:
             continue
-        loop_continue = False
-        # check X
-        if change_check:
-            for j in range(now_c + 1, next_c + 1):
-                print(park[now_r][j])
-                if park[now_r][j] == "X":
-                    print("find X in col route")
-                    loop_continue = True
-                    break
         else:
-            for j in range(now_r + 1, next_r + 1):
-                print(park[j][now_c])
-                if park[j][now_c] == "X":
-                    print("find X in row route")
-                    loop_continue = True
-                    break
-        if loop_continue is True:
-            continue
-        now_c = next_c
-        now_r = next_r
-        print("can go")
+            now_r = next_r
+            now_c = next_c
                 
     answer = [now_r, now_c]
     return answer
