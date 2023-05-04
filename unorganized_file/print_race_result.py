3801dd1542bd3502ccbd6d6cc7a3df399e9d7dba
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-05-04 15:50:43 +0900

    Edit code using deque
    With more time consuming

--- unorganized_file/print_race_result.py
+++ unorganized_file/print_race_result.py
@@ -1,10 +1,25 @@
 # Lv.1
 def solution(players, callings):
-    for i in range(len(callings)):
-        faster = callings[i]
-        faster_i = players.index(faster)
-        later = players[faster_i - 1]
-        players[faster_i - 1] = faster
-        players[faster_i] = later
-    answer = players
+    from collections import deque
+    temp = callings[0]
+    count = 1
+    commands = []
+    for i in range(1, len(callings)):
+        if callings[i] == temp:
+            count += 1
+        else:
+            commands.append([temp, count])
+            count = 1
+            temp = callings[i]
+    commands.append([temp, count])
+    # print(commands)
+    que = deque(players)
+    for i in range(len(commands)):
+        faster = commands[i][0]
+        count = commands[i][1]
+        current_i = que.index(faster)
+        que.remove(faster)
+        que.insert(current_i - count, faster)
+        # print(que)
+    answer = list(que)
     return answer
