bbc3adcd2774d4746b823348e51056f9901172dd
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-16 11:40:57 +0900

    Solve problems by using dictionary

--- programmers/Lv.1/print_race_result.py
+++ programmers/Lv.1/print_race_result.py
@@ -2,26 +2,22 @@
 # 달리기 경주
 
 def solution(players, callings):
-    from collections import deque
-    temp = callings[0]
-    count = 1
-    commands = []
-    for i in range(1, len(callings)):
-        if callings[i] == temp:
-            count += 1
-        else:
-            commands.append([temp, count])
-            count = 1
-            temp = callings[i]
-    commands.append([temp, count])
-    # print(commands)
-    que = deque(players)
-    for i in range(len(commands)):
-        faster = commands[i][0]
-        count = commands[i][1]
-        current_i = que.index(faster)
-        que.remove(faster)
-        que.insert(current_i - count, faster)
-        # print(que)
-    answer = list(que)
+    players_dic = {} # key: 선수, value: 등수
+    scores_dic = {} # key: 등수, value: 선수
+    for i in range(len(players)):
+        players_dic[players[i]] = i
+        scores_dic[i] = players[i]
+    # print(players_dic)
+    # print(scores_dic)
+    for calling in callings:
+        now = players_dic[calling]
+        # 순위에 따른 선수 수정
+        scores_dic[now] = scores_dic[now - 1]
+        scores_dic[now - 1] = calling
+        # 선수의 등수 수정
+        players_dic[calling] -= 1
+        players_dic[scores_dic[now]] += 1
+        # print(players_dic)
+        # print(scores_dic)
+    answer = list(scores_dic.values())
     return answer
