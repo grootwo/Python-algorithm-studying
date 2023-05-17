1d41654ae1472cbb1fd93d58c307a0415fb598ad
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-05-17 12:23:45 +0900

    Fix the way checking a character

--- programmers/Lv.1/decode_string.py
+++ programmers/Lv.1/decode_string.py
@@ -1,20 +1,19 @@
 def solution(s, skip, index):
-    # how to initate alphabet
     answer = []
     for cha in s:
         now_i = ord(cha)
         check = 1
         i = 1
         while check <= index:
-            if chr(now_i + i) in skip:
+            if now_i + i > 122:
+                i -= 26
+            next_cha = chr(now_i + i)
+            if next_cha in skip:
                 i += 1
                 continue
             else:
                 i += 1
                 check += 1
-        if now_i + i > 122:
-            answer.append(chr(now_i + i - 27))
-        else:
-            answer.append(chr(now_i + i - 1))
+        answer.append(chr(now_i + i - 1))
     answer = ''.join(answer)
     return answer
