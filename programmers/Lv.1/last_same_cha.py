cedcd47b7625efb6b5e0bb8fcd1e2c3fbd93189c
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-05-22 12:19:28 +0900

    Create last_same_cha.py

---
+++ programmers/Lv.1/last_same_cha.py
@@ -0,0 +1,12 @@
+def solution(s):
+    last_location_dic = {}
+    answer = []
+    for i in range(len(s)):
+        cha = s[i]
+        if cha not in last_location_dic:
+            last_location_dic[cha] = i
+            answer.append(-1)
+        else:
+            answer.append(i - last_location_dic[cha])
+            last_location_dic[cha] = i
+    return answer
