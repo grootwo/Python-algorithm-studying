1a7a9e727e15f8709a60ae9a2a8f4532f0c8760c
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-23 21:13:21 +0900

    Add comment

--- programmers/Lv.1/devide_string.py
+++ programmers/Lv.1/devide_string.py
@@ -1,3 +1,5 @@
+# LV.1
+# 문자열 나누기
 def solution(s):
     first_cha = s[0]
     start = 0
@@ -6,8 +8,8 @@ def solution(s):
     answer = []
     for i in range(1, len(s)):
         if first_cha_count == other_cha_count:
-            answer.append(s[start:i])
-            first_cha = s[i]
+            answer.append(s[start:i]) # 문자열 추가하기
+            first_cha = s[i] # 다음 문자부터 인덱싱하기
             first_cha_count = 0
             other_cha_count = 0
             start = i
