76297a8faf051db2d939412bae910c9cbe75507a
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-24 23:18:17 +0900

    Change deque into list

--- programmers/Lv.1/honored_list.py
+++ programmers/Lv.1/honored_list.py
@@ -1,16 +1,18 @@
 def solution(k, score):
     from collections import deque
-    honor_list = deque([])
+    honor_list = []
     least_score = []
-    for new_score in range(k):
+    for i in range(k):
+        new_score = score[i]
         honor_list.append(new_score)
         least_score.append(min(honor_list))
     honor_list.sort()
-    for new_score in range(k, len(score)):
+    honor_list.reverse()
+    for i in range(k, len(score)):
+        new_score = score[i]
         if new_score > min(honor_list):
-            honor_list.pop()
-            honor_list.append(new_score)
-            honor_list.sort()
+            honor_list[-1] = new_score
+        honor_list.sort()
+        honor_list.reverse()
         least_score.append(min(honor_list))
     return least_score
-
