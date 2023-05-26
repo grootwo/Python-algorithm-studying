56f93a7eca53e71895d279c7663846ed8d394f57
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-26 23:34:46 +0900

    Reduce unnecessary codes

--- programmers/Lv.1/honored_list.py
+++ programmers/Lv.1/honored_list.py
@@ -6,11 +6,8 @@ def solution(k, score):
     least_score = []
     for i in range(len(score)):
         new_score = score[i]
-        if i < k:
-            honor_list.append(new_score)
-        else:
-            if new_score > min(honor_list):
-                honor_list.remove(min(honor_list))
-                honor_list.append(new_score)
+        honor_list.append(new_score)
+        if i >= k:
+            honor_list.remove(min(honor_list))
         least_score.append(min(honor_list))
     return least_score
