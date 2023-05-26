7131824926a7173ad5cd20826eef65716fb0fd6c
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-26 23:27:10 +0900

    Delete code sorting

--- programmers/Lv.1/honored_list.py
+++ programmers/Lv.1/honored_list.py
@@ -6,13 +6,10 @@ def solution(k, score):
         new_score = score[i]
         honor_list.append(new_score)
         least_score.append(min(honor_list))
-    honor_list.sort()
-    honor_list.reverse()
     for i in range(k, len(score)):
         new_score = score[i]
         if new_score > min(honor_list):
-            honor_list[-1] = new_score
-        honor_list.sort()
-        honor_list.reverse()
+            honor_list.remove(min(honor_list))
+            honor_list.append(new_score)
         least_score.append(min(honor_list))
     return least_score
