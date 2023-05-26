2cd6c25b36127f33963315b3692dd78f438f4e47
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-05-26 23:31:49 +0900

    Edit inside of for loop statement

--- programmers/Lv.1/honored_list.py
+++ programmers/Lv.1/honored_list.py
@@ -1,15 +1,16 @@
+# Lv.1
+# 명예의 전당 (1)
 def solution(k, score):
     from collections import deque
     honor_list = []
     least_score = []
-    for i in range(k):
+    for i in range(len(score)):
         new_score = score[i]
-        honor_list.append(new_score)
-        least_score.append(min(honor_list))
-    for i in range(k, len(score)):
-        new_score = score[i]
-        if new_score > min(honor_list):
-            honor_list.remove(min(honor_list))
+        if i < k:
             honor_list.append(new_score)
+        else:
+            if new_score > min(honor_list):
+                honor_list.remove(min(honor_list))
+                honor_list.append(new_score)
         least_score.append(min(honor_list))
     return least_score
