3a314a44e5dd2c666d17ada90a84305af6f467e1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-01 13:35:46 +0900

    Create make_burgers_with_stack.py

---
+++ programmers/Lv.1/make_burgers_with_stack.py
@@ -0,0 +1,13 @@
+from collections import deque
+def solution(ingredient):
+    que = deque([])
+    count = 0
+    for ing in ingredient:
+        que.append(ing)
+        if que[-1] == 1 and len(que) >= 4:
+            if que[-4] == 1 and que[-3] == 2 and que[-2] == 3:
+                for i in range(4):
+                    que.pop()
+                count += 1
+    answer = count
+    return answer
