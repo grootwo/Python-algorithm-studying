35a7849478d2f85bffe452465215703cea553b31
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-04 22:55:26 +0900

    Change range checking divisor

--- programmers/Lv.1/knight_weapons.py
+++ programmers/Lv.1/knight_weapons.py
@@ -1,9 +1,14 @@
+# Lv.1
+# 기사단원의 무기
 def solution(number, limit, power):
     answer = [0] * number
     for i in range(1, number + 1):
-        for num in range(1, number + 1):
-            if num % i == 0:
-                answer[num - 1] += 1
+        for j in range(1, int(i ** (1/2) + 1)):
+            if i % j == 0 and j * j != i:
+                answer[i - 1] += 2
+            elif j * j == i:
+                answer[i - 1] += 1
+    # print(answer)
     for i in range(number):
         if answer[i] > limit:
             answer[i] = power
