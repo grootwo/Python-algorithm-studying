8fb1c31ce15a4d4df9b10a2316254fe6446e3850
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-06 21:39:01 +0900

    Add comments

--- programmers/Lv.1/make_num_with_same_int.py
+++ programmers/Lv.1/make_num_with_same_int.py
@@ -8,19 +8,21 @@ def solution(X, Y):
     # print(X)
     # print(Y)
     for num in X:
-        if num in Y:
+        if num in Y: # 만약 겹치는 숫자라면
+            # 해당 숫자와 둘 중 적은 숫자를 기록
             if X[num] > Y[num]:
                 same_num.append([int(num), Y[num]])
             else:
                 same_num.append([int(num), X[num]])
     same_num.sort(key=lambda x: (-x[0]))
     answer = ''
+    # 문자열 만들기
     if same_num and same_num[0][0] != 0:
         for nums in same_num:
             answer += str(nums[0]) * nums[1]
-    elif same_num:
+    elif same_num: # 0만 있다면
         answer = '0'
-    else:
+    else: # 겹치는 숫자가 없다면
         answer = '-1'
     # print(answer)
     return answer
