a0cf5d7a52c4671b4a4ece9e334041ad022714c9
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-08 21:22:37 +0900

    Add code adding report mail's count

--- programmers/Lv.1/report_result_email.py
+++ programmers/Lv.1/report_result_email.py
@@ -5,7 +5,10 @@ def solution(id_list, report, k):
     for i in range(len(report)):
         user_reporting, user_reported = report[i].split(" ")
         user_report_dic[user_reported].append(user_reporting)
-#     for user_id in id_list:
-        
+    for user_id in id_list:
+        if len(user_report_dic[user_id]) - 1 >= k:
+            for user_to_email in user_report_dic[user_id][1:]:
+                user_report_dic[user_to_email][0] += 1
+    print(user_report_dic)
     answer = []
     return answer
