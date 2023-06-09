87095f3ec80c89e99e97d8a75fc82e8c586bc0fd
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-09 22:39:47 +0900

    Add code checking same user and returning result

--- programmers/Lv.1/report_result_email.py
+++ programmers/Lv.1/report_result_email.py
@@ -4,11 +4,15 @@ def solution(id_list, report, k):
         user_report_dic[user_id] = [0]
     for i in range(len(report)):
         user_reporting, user_reported = report[i].split(" ")
-        user_report_dic[user_reported].append(user_reporting)
+        if user_reporting not in user_report_dic[user_reported]:
+            user_report_dic[user_reported].append(user_reporting)
+    answer = []
     for user_id in id_list:
         if len(user_report_dic[user_id]) - 1 >= k:
             for user_to_email in user_report_dic[user_id][1:]:
                 user_report_dic[user_to_email][0] += 1
-    print(user_report_dic)
-    answer = []
+    for user_id in id_list:
+        answer.append(user_report_dic[user_id][0])
+        # print(user_report_dic[user_id])
+    # print(user_report_dic)
     return answer
