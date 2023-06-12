f60bab64c489e4150de70e03a413b38c742215d0
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-12 21:15:48 +0900

    Update min_card_wallet.py

--- programmers/Lv.1/min_card_wallet.py
+++ programmers/Lv.1/min_card_wallet.py
@@ -3,9 +3,9 @@
 def solution(sizes):
     max_w = 0
     max_h = 0
-    for i in range(len(sizes)):
-        x = min(sizes[i])
-        y = max(sizes[i])
+    for size in sizes:
+        x = min(size)
+        y = max(size)
         if x > max_w:
             max_w = x
         if y > max_h:
