03aa27c3c9699262382be6a5eb8d940d694cd3ea
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-14 21:49:25 +0900

    Add comments

--- programmers/Lv.1/calculate_lack_of_money.py
+++ programmers/Lv.1/calculate_lack_of_money.py
@@ -1,4 +1,7 @@
+# Lv.1
+# 부족한 금액 계산하기
 def solution(price, money, count):
+    # 등차수열을 이용하여 총 비용 계산하기
     total_price = count * (price + price * count) // 2
     if total_price <= money:
         answer = 0
