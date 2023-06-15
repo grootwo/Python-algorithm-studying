ad8df9b323ef840b9348971cb63f742379c19eb3
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-15 21:56:38 +0900

    Create num_n_string.py

---
+++ programmers/Lv.1/num_n_string.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(s):
+    count_dic = {
+        'zer': [4, '0'],
+        'one': [3, '1'],
+        'two': [3, '2'],
+        'thr': [5, '3'],
+        'fou': [4, '4'],
+        'fiv': [4, '5'],
+        'six': [3, '6'],
+        'sev': [5, '7'],
+        'eig': [5, '8'],
+        'nin': [4, '9']
+    }
+    answer = ''
+    que = deque(s)
+    while que:
+        if ord(que[0]) >= 97:
+            print(que[0:3])
+            cha = count_dic[que[0:3]]
+            for i in range(cha[0]):
+                que.popleft()
+            answer += cha[1]
+        else:
+            answer += que.popleft()
+    answer = int(answer)
+    return answer
