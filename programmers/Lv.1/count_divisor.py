ca0f00f08e3863e391399412b62f3ab293ffb955
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-16 17:35:08 +0900

    Add comments

--- programmers/Lv.1/count_divisor.py
+++ programmers/Lv.1/count_divisor.py
@@ -1,10 +1,13 @@
+# Lv.1
+# 약수의 개수와 덧셈
 def solution(left, right):
     int_list = [0] * (right - left + 1)
+    # 약수의 개수 구하기
     for i in range(1, right + 1):
         for j in range(left, right + 1):
             if j >= i and j % i == 0:
                 int_list[j - left] += 1
-    # print(int_list[left : right + 1])
+    # 결괏값 구하기
     answer = 0
     for i in range(left, right + 1):
         if int_list[i - left] % 2 == 0:
