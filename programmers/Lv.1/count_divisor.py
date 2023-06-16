a7081f93b7b2fc583b076a7c06a4c54c21cc806b
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-16 17:33:51 +0900

    Change code not making big list

--- programmers/Lv.1/count_divisor.py
+++ programmers/Lv.1/count_divisor.py
@@ -1,13 +1,13 @@
 def solution(left, right):
-    int_list = [0] * 1000
+    int_list = [0] * (right - left + 1)
     for i in range(1, right + 1):
         for j in range(left, right + 1):
             if j >= i and j % i == 0:
-                int_list[j] += 1
+                int_list[j - left] += 1
     # print(int_list[left : right + 1])
     answer = 0
     for i in range(left, right + 1):
-        if int_list[i] % 2 == 0:
+        if int_list[i - left] % 2 == 0:
             answer += i
         else:
             answer -= i
