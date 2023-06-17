49b62932d71550d297c3cb5a4da3e6bf096cf939
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-17 19:31:21 +0900

    Fix error and add comments

--- programmers/Lv.1/get_min_max_lottery_score.py
+++ programmers/Lv.1/get_min_max_lottery_score.py
@@ -1,4 +1,7 @@
+# Lv.1
+# 로또의 최고 순위와 최저 순위
 def solution(lottos, win_nums):
+    # 일치하는 숫자와 모르는 숫자 세기
     count_correct = 0
     count_none = 0
     for num in lottos:
@@ -6,8 +9,8 @@ def solution(lottos, win_nums):
             count_none += 1
         elif num in win_nums:
             count_correct += 1
+    # 등수 확인하기
     answer = [count_correct + count_none, count_correct]
     for i in range(2):
-        answer[i] = 7 - answer[i]
-        print(answer[i])
+        answer[i] = 7 - max(answer[i], 1) # 1개나 0개 맞았을 때 결과가 같도록
     return answer
