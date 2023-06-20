6633fd171daa3382b38d42c036b1112bb68c08b0
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-20 16:57:37 +0900

    Complete step 2

--- programmers/Lv.1/recommend_new_id.py
+++ programmers/Lv.1/recommend_new_id.py
@@ -1,10 +1,18 @@
 def solution(new_id):
+    # 1
+    new_id = list(new_id.lower())
+    # 2
     allowed_symbols = ['-', '_', '.']
-    new_id = new_id.lower()
-    for cha in new_id:
+    remove_count = 0
+    for i in range(len(new_id)):
+        cha = new_id[i]
         cha_o = ord(cha)
-        print(cha_o)
-        if cha_o < 48 or 57 < cha_o < 97 or 122 < cha_o or cha_o not in allowed_symbols:
-            print('no', cha)
+        if cha_o < 48 or 57 < cha_o < 97 or 122 < cha_o:
+            if cha not in allowed_symbols:
+                new_id[i] = 0
+                remove_count += 1
+    for i in range(remove_count):
+        new_id.remove(0)
+    # 3
     answer = ''
     return answer
