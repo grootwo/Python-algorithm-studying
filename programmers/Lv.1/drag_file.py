007b4781532f44913f4712413ce7c93a5764f1a4
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-22 10:40:47 +0900

    Change the way to select min, max values

--- programmers/Lv.1/drag_file.py
+++ programmers/Lv.1/drag_file.py
@@ -1,24 +1,12 @@
 # Lv.1
 # 바탕화면 정리
 def solution(wallpaper):
-    row = len(wallpaper)
-    col = len(wallpaper[0])
-    min_r = row
-    min_c = col
-    max_r = 0
-    max_c = 0
-    flag = False
-    for i in range(row):
-        for j in range(col):
-            if wallpaper[i][j] == "#":
-                if i < min_r:
-                    min_r = i
-                if j < min_c:
-                    min_c = j
-                if i > max_r:
-                    max_r = i
-                if j > max_c:
-                    max_c = j
-    # print(min_r, min_c, max_r, max_c)
-    answer = [min_r, min_c, max_r + 1, max_c + 1]
+    cols = []
+    rows = []
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == '#':
+                cols.append(j)
+                rows.append(i)
+    answer = [min(rows), min(cols), max(rows) + 1, max(cols) + 1]
     return answer
