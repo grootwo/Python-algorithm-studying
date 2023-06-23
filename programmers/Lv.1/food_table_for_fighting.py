6f637664691869c75ab5f565539bfbacb0d0cf58
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-23 10:08:23 +0900

    Edit comments

--- programmers/Lv.1/food_table_for_fighting.py
+++ programmers/Lv.1/food_table_for_fighting.py
@@ -2,19 +2,20 @@
 # 푸드 파이트 대회
 def solution(food):
     food_table = []
+    # 한 명이 먹을 음식 양으로 정리하기
     for i in range(len(food)):
         food[i] = food[i] // 2
-    # print(food_table)
-    # print(food)
+    # 좌측부터 순서대로 테이블 정렬하기
     for i in range(1, len(food)):
         for j in range(food[i]):
             food_table.append(i)
+    # 물 배치하기
     food_table.append(0)
+    # 우측 테이블 정렬하기
     food.reverse()
     for i in range(len(food)):
         for j in range(food[i]):
             food_table.append(len(food) - 1 - i)
-    # print(food_table)
     food_table = list(map(str, food_table))
     answer = ''.join(food_table)
     return answer
