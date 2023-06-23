5608ef0951ee1df42b339bb9cbcd249f0b7a9a2e
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-23 10:09:18 +0900

    Move a line

--- programmers/Lv.1/food_table_for_fighting.py
+++ programmers/Lv.1/food_table_for_fighting.py
@@ -1,11 +1,11 @@
 # Lv.1
 # 푸드 파이트 대회
 def solution(food):
-    food_table = []
     # 한 명이 먹을 음식 양으로 정리하기
     for i in range(len(food)):
         food[i] = food[i] // 2
     # 좌측부터 순서대로 테이블 정렬하기
+    food_table = []
     for i in range(1, len(food)):
         for j in range(food[i]):
             food_table.append(i)
