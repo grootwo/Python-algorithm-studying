9b61cce1bcbcc163f694028a8569dcd8d7d61dcd
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-28 21:53:54 +0900

    Create sum_combination.py

---
+++ programmers/Lv.1/sum_combination.py
@@ -0,0 +1,11 @@
+# Lv.1
+# 두 개 뽑아서 더하기
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            num = numbers[i] + numbers[j]
+            if num not in answer:
+                answer.append(num)
+    answer.sort()
+    return answer
