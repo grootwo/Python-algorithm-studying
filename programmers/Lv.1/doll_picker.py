f53b776b1f0251ada23db81e87d5942c8153f10f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-06-30 19:55:09 +0900

    Create doll_picker.py

---
+++ programmers/Lv.1/doll_picker.py
@@ -0,0 +1,12 @@
+def solution(board, moves):
+    stack = deque([])
+    count = 0
+    for move in moves:
+        
+    return count
+
+def get_top_doll(col, board):
+    for i in range(len(board)):
+        if board[i][row] != 0:
+            return board[i][row]
+    return 0
