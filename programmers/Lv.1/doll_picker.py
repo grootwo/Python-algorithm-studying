b6d15c98d8969d05c7939f1eb9e808d5d2f5476b
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-01 22:55:38 +0900

    Update doll_picker.py
    With index out of range error

--- programmers/Lv.1/doll_picker.py
+++ programmers/Lv.1/doll_picker.py
@@ -1,12 +1,18 @@
+from collections import deque
 def solution(board, moves):
     stack = deque([])
     count = 0
     for move in moves:
-        
+        doll = get_top_doll(move, board)
+        if len(stack) > 1 and stack[-1] == doll:
+            stack.pop()
+            count += 1
+        else:
+            stack.append(doll)
     return count
 
 def get_top_doll(col, board):
     for i in range(len(board)):
-        if board[i][row] != 0:
-            return board[i][row]
+        if board[i][col] != 0:
+            return board[i][col]
     return 0
