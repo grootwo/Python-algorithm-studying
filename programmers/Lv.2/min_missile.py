f160cc1213dd7d4eb55cdbd6375ae7b2a8c283d4
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-04 23:20:54 +0900

    Update min_missile.py

--- programmers/Lv.2/min_missile.py
+++ programmers/Lv.2/min_missile.py
@@ -1,6 +1,22 @@
 def solution(targets):
-    targets.sort(key=lambda x: -x[1])
-    max_num = targets[0][1]
-    print(max_num)
-    answer = 0
-    return answer
+    count = 0
+    while targets:
+        max_location = get_max_location(targets)
+        for target in targets:
+            if include_location(max_location, target):
+                targets.remove(target)
+            count += 1
+    return count
+
+def get_max_location(locations):
+    locations.sort(key=lambda x: -x[1])
+    location_list = [0] * locations[0][1]
+    for location in locations:
+        for i in range(location[0], location[1]):
+            location_list[i] += 1
+    return location_list.index(max(location_list))
+
+def include_location(x, location):
+    if location[0] <= x < location[1]:
+        return True
+    return False
