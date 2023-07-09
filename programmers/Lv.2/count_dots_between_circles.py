e73a515a67ead209bc2b365dc8ad7bf6871da60d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-09 22:59:04 +0900

    Debug by writing repeat statement

--- programmers/Lv.2/count_dots_between_circles.py
+++ programmers/Lv.2/count_dots_between_circles.py
@@ -2,8 +2,11 @@ import math
 def solution(r1, r2):
     count = 0
     for i in range(r2 + 1):
-        for j in range(r1 + 1):
-            print(i, j)
-            print(math.floor(math.sqrt(i)) - math.floor(math.sqrt(j)))
-    print(count)
+        for j in range(r2 + 1):
+            if i ** 2 + j ** 2 <= r2 ** 2:
+                count += 1
+    # for i in range(r2 + 1):
+    #     for j in range(r1 + 1):
+    #         count += math.floor(math.sqrt(abs(i ** 2 - j ** 2)))
+    #         print(count)
     return count
