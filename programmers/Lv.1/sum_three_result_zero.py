3c453dc88b5a2af878c9933dcdc437d06a1a8e67
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-10 10:32:57 +0900

    Create sum_three_result_zero.py

---
+++ programmers/Lv.1/sum_three_result_zero.py
@@ -0,0 +1,12 @@
+# Lv.1
+# 삼총사
+def solution(number):
+    length = len(number)
+    count = 0
+    for i in range(length):
+        for j in range(i + 1, length):
+            for k in range(j + 1, length):
+                if number[i] + number[j] + number[k] == 0:
+                    count += 1
+    answer = count
+    return answer
