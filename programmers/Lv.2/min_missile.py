1e98972cf1a39b92c5aaf25073c3c917a67765aa
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-11 23:11:28 +0900

    Update min_missile.py

--- programmers/Lv.2/min_missile.py
+++ programmers/Lv.2/min_missile.py
@@ -1,3 +1,5 @@
+# Lv.2
+# 요격 시스템
 def solution(targets):
     answer = 0
     targets.sort(key=lambda x:x[1])
@@ -8,5 +10,4 @@ def solution(targets):
         if s >= end:  # 이전에 요격한 미사일로는 현재 미사일을 요격할 수 없는 경우
             answer += 1
             end = e  # 현재 미사일을 요격함으로써 끝나는 지점을 업데이트
-
     return answer
