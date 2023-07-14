efe3ec77accddabf5298aedda2f87da384a6f4e6
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-14 23:04:50 +0900

    Add comments

--- programmers/Lv.2/assignment_process.py
+++ programmers/Lv.2/assignment_process.py
@@ -1,10 +1,14 @@
 from collections import deque
 def solution(plans):
-    # 시작 시간 별로 정리
+    # 시작 시간 별로 정리: sort
+    # 다음 시작 전까지 완료: - time
+    # 남은 시간 저장: append
+    # 다음 시작 전까지 완료: - time
+    # 남은 과제 하기: popleft
+    # 다음 시작 전까지 완료: - time
     plans.sort(key=lambda x:(x[1]))
     left_plans = deque(plans)
     unsolved_plans = deque([])
-    answer
     now = left_plans[0]
     while len(left_plans) != 0 and len(unsolved_plans) != 0:
         print(hi)
