4d960dd8347a6a3502d2f3e19dbf3d84e699ce89
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-16 23:39:10 +0900

    Update failure_percent.py with division zero error

--- programmers/Lv.1/failure_percent.py
+++ programmers/Lv.1/failure_percent.py
@@ -1,11 +1,10 @@
 def solution(N, stages):
-    stage_people = {}
+    stage_score = [0] * (N + 2)
     for stage in stages:
-        if stage in stage_people:
-            stage_people[stage] += 1
-        else:
-            stage_people[stage] = 1
+        stage_score[stage] += 1 
+    stage_failure = []
+    for i in range(1, N + 2):
+        stage_failure.append(stage_score[i] / sum(stage_score[i:N + 2]))
+    print(stage_failure)
     answer = []
-    for stage in list(stage_people.keys()):
-        # 여기서 정렬하려면 list의 sort가 나아보인다.
     return answer
