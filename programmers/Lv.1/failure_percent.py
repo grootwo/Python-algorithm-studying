b29f3e15e692dcd4bd810f2763af697de0018300
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-17 22:35:23 +0900

    Fix division error by changing range
    But need to fix 2 dimension array's index

--- programmers/Lv.1/failure_percent.py
+++ programmers/Lv.1/failure_percent.py
@@ -1,10 +1,11 @@
 def solution(N, stages):
-    stage_score = [0] * (N + 2)
+    stage_score = []
+    for i in range(N + 2):
+        stage_score.append([i, 0, 0])
     for stage in stages:
-        stage_score[stage] += 1 
-    stage_failure = []
-    for i in range(1, N + 2):
-        stage_failure.append(stage_score[i] / sum(stage_score[i:N + 2]))
-    print(stage_failure)
+        stage_score[stage][1] += 1
+    for i in range(1, N + 1):
+        stage_score[stage][2] = stage_score[i][1] / sum(stage_score[i:N + 2])
+    print(stage_score)
     answer = []
     return answer
