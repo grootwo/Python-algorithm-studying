03aaedaf521700995baf7e3db61f626f0386d036
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-18 23:26:00 +0900

    Update failure_percent.py

--- programmers/Lv.1/failure_percent.py
+++ programmers/Lv.1/failure_percent.py
@@ -1,11 +1,12 @@
 def solution(N, stages):
-    stage_score = []
-    for i in range(N + 2):
-        stage_score.append([i, 0, 0])
+    # stage별 도달 참가자 수 구하기
+    stage_score = [0] * (N + 2)
     for stage in stages:
-        stage_score[stage][1] += 1
+        stage_score[stage] += 1
+    # 각 stage별 실패율과 stage 저장하기
+    stage_info = []
     for i in range(1, N + 1):
-        stage_score[stage][2] = stage_score[i][1] / sum(stage_score[i:N + 2])
-    print(stage_score)
-    answer = []
-    return answer
+        stage_info.append([i, stage_score[i] / sum(stage_score[i:N + 2])])
+    # 실패율을 기준으로 내림차순으로 정리
+    stage_info.sort(key=lambda x:-x[1])
+    return [i[0] for i in stage_info]
