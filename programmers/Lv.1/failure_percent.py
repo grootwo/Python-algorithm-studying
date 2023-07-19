88bc6b02ecb26e289e6c7fab7805719deafcf5fa
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-19 23:52:18 +0900

    Add other's solution as comments

--- programmers/Lv.1/failure_percent.py
+++ programmers/Lv.1/failure_percent.py
@@ -10,3 +10,24 @@ def solution(N, stages):
     # 실패율을 기준으로 내림차순으로 정리
     stage_info.sort(key=lambda x:-x[1])
     return [i[0] for i in stage_info]
+
+# def solution(N, stages):
+#     failure_rate = {}
+#     stages_len = len(stages)
+
+#     for i in range(1, N+1):
+#         if stages_len != 0:
+#             stage_count = stages.count(i)
+#             failure_rate[i] = stage_count/stages_len
+#             stages_len -= stage_count
+#         else:
+#             failure_rate[i] = 0
+
+#     answer = sorted(failure_rate.keys(), key=lambda key: failure_rate[key], reverse=True)
+
+#     return answer
+
+# N = 5
+# stages = [2, 1, 2, 6, 2, 4, 3, 3]
+
+# print(solution(N, stages))
