af913cba5a9c1dec618405bdbc8055095612531f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-23 23:11:42 +0900

    Change the way to delete same students

--- programmers/Lv.1/max_training_cloth.py
+++ programmers/Lv.1/max_training_cloth.py
@@ -1,13 +1,9 @@
 def solution(n, lost, reserve):
-    count = 0
-    for num in lost:
-        if num in reserve:
-            reserve.remove(num)
-            count += 1
-        elif num - 1 in reserve:
-            reserve.remove(num - 1)
-            count += 1
-        elif num + 1 in reserve:
-            reserve.remove(num + 1)
-            count += 1
-    return n - len(lost) + count
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for num in set_reserve:
+        if num - 1 in set_lost:
+            set_lost.remove(num - 1)
+        elif num + 1 in set_lost:
+            set_lost.remove(num + 1)
+    return n - len(set_lost)
