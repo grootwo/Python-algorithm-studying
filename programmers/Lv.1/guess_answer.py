427ce86ebdfc037f53cfd03a94600c168d2024d0
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-24 23:21:10 +0900

    Create guess_answer.py

---
+++ programmers/Lv.1/guess_answer.py
@@ -0,0 +1,13 @@
+def solution(answers):
+    answer_dic = {0: [1, 2, 3, 4, 5], 1: [2, 1, 2, 3, 2, 4, 2, 5], 2: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]}
+    scores = [0, 0, 0]
+    for i in range(len(answers)):
+        answer = answers[i]
+        if answer == answer_dic[0][i // 5]:
+            scores[0] += 1
+        if answer == answer_dic[1][i // 8]:
+            scores[1] += 1
+        if answer == answer_dic[2][i // 10]:
+            scores[2] += 1
+    print(scores)
+    return scores
