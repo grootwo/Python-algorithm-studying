8085cddc9a83615dc72fe244c553a0de1fcf8f77
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-25 23:13:52 +0900

    Fix index

--- programmers/Lv.1/guess_answer.py
+++ programmers/Lv.1/guess_answer.py
@@ -3,18 +3,11 @@ def solution(answers):
     scores = [0, 0, 0]
     for i in range(len(answers)):
         answer = answers[i]
-        print('answer: ', answer)
-        print('0:', answer_dic['0'][i // 5])
-        print('1:', answer_dic['1'][i // 8])
-        print('2:', answer_dic['2'][i // 10])
-        if answer == answer_dic['0'][i // 5]:
+        if answer == answer_dic['0'][i % 5]:
             scores[0] += 1
-            print(0)
-        if answer == answer_dic['1'][i // 8]:
+        if answer == answer_dic['1'][i % 8]:
             scores[1] += 1
-            print(1)
-        if answer == answer_dic['2'][i // 10]:
+        if answer == answer_dic['2'][i % 10]:
             scores[2] += 1
-            print(2)
     print(scores)
     return scores
