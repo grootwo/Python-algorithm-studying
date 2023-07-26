4861443922a8abc5aa16a87cb62127b3033f2165
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-26 22:48:24 +0900

    Change str key to int key

--- programmers/Lv.1/guess_answer.py
+++ programmers/Lv.1/guess_answer.py
@@ -1,13 +1,13 @@
 def solution(answers):
-    answer_dic = {'0': [1, 2, 3, 4, 5], '1': [2, 1, 2, 3, 2, 4, 2, 5], '2': [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]}
+    answer_dic = {0: [1, 2, 3, 4, 5], 1: [2, 1, 2, 3, 2, 4, 2, 5], 2: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]}
     scores = [0, 0, 0]
     for i in range(len(answers)):
         answer = answers[i]
-        if answer == answer_dic['0'][i % 5]:
+        if answer == answer_dic[0][i % 5]:
             scores[0] += 1
-        if answer == answer_dic['1'][i % 8]:
+        if answer == answer_dic[1][i % 8]:
             scores[1] += 1
-        if answer == answer_dic['2'][i % 10]:
+        if answer == answer_dic[2][i % 10]:
             scores[2] += 1
     print(scores)
     print(scores.index(max(scores)))
