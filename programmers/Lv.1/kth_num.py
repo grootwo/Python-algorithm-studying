9fc938c2cce1ccf869f00393b40ad98638d15be3
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-27 23:01:06 +0900

    Reduce code

--- programmers/Lv.1/kth_num.py
+++ programmers/Lv.1/kth_num.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        temp = array[command[0] - 1:command[1]]
-        temp.sort()
+        temp = sorted(array[command[0] - 1:command[1]])
         answer.append(temp[command[2] - 1])
     return answer
