e4d13143947927fb4ddae0dfbfa82556a3f430e1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-28 22:38:03 +0900

    Add code checking same names

--- programmers/Lv.1/uncompleted_runner.py
+++ programmers/Lv.1/uncompleted_runner.py
@@ -2,3 +2,6 @@ def solution(participant, completion):
     for name in participant:
         if name not in completion:
             return name
+        else:
+            completion.remove(name)
+    return completion[0]
