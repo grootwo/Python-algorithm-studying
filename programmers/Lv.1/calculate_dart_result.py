9878f34b892b2f785c9fd21df856e3e10928c3a4
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-30 21:19:17 +0900

    Create calculate_dart_result.py

---
+++ programmers/Lv.1/calculate_dart_result.py
@@ -0,0 +1,11 @@
+def solution(dartResult):
+    answer_1st = ''
+    answer_2nd = ''
+    answer_3rd = ''
+    for i in dartResult:
+        # 정수인지 확인
+        # 정수라면 각 정답에 더하기
+        # SDT에 따라 곱셈 추가
+        # 옵션에 따라 마지막에 곱셈 추가
+    answer = 0
+    return answer
