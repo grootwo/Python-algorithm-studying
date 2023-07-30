c02d517a81923e7372f97ef1717bc6fc0f6e1770
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-30 21:21:46 +0900

    Change answer as list

--- programmers/Lv.1/calculate_dart_result.py
+++ programmers/Lv.1/calculate_dart_result.py
@@ -1,9 +1,8 @@
 def solution(dartResult):
-    answer_1st = ''
-    answer_2nd = ''
-    answer_3rd = ''
+    answer = ['', '', '']
     for i in dartResult:
         # 정수인지 확인
+        # 정수를 count하며 1, 2, 3에 더하기
         # 정수라면 각 정답에 더하기
         # SDT에 따라 곱셈 추가
         # 옵션에 따라 마지막에 곱셈 추가
