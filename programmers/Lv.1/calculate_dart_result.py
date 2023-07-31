055b176b1e737912224387ba191417a1a6dd4599
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-31 21:55:45 +0900

    Add code checking int cha

--- programmers/Lv.1/calculate_dart_result.py
+++ programmers/Lv.1/calculate_dart_result.py
@@ -1,10 +1,15 @@
 def solution(dartResult):
     answer = ['', '', '']
+    check = -1
     for i in dartResult:
+        # 정수라면
+        if '0' <= i <= '9':
+            check += 1
+            answer[check] += i
         # 정수인지 확인
         # 정수를 count하며 1, 2, 3에 더하기
         # 정수라면 각 정답에 더하기
         # SDT에 따라 곱셈 추가
         # 옵션에 따라 마지막에 곱셈 추가
-    answer = 0
+    print(answer)
     return answer
