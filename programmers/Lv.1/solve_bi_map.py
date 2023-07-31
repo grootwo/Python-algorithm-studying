86eb096024c543412b3b7273bd87cb0efd9e81b7
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-31 22:24:58 +0900

    Return answer as list

--- programmers/Lv.1/solve_bi_map.py
+++ programmers/Lv.1/solve_bi_map.py
@@ -1,9 +1,16 @@
 def solution(n, arr1, arr2):
-    for i in arr1:
-        print(i, ':')
-        print(get_bi_str(i, n))
-    answer = []
-    return answer
+    for i in range(5):
+        arr1[i] = get_bi_str(arr1[i], n)
+        arr2[i] = get_bi_str(arr2[i], n)
+    result = [[0] * n for _ in range(n)]
+    print(result)
+    for i in range(5):
+        for j in range(5):
+            if arr1[i][j] == 0 and arr2[i][j] == 0:
+                result[i][j] = ' '
+            else:
+                result[i][j] = '#'
+    return result
 
 def get_bi_str(num, n):
     answer = ''
