8acb66967925d7246bf4821ab5a6b4ea64f24b5c
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-07-31 22:26:49 +0900

    Change actual number to variable n

--- programmers/Lv.1/solve_bi_map.py
+++ programmers/Lv.1/solve_bi_map.py
@@ -1,15 +1,15 @@
 def solution(n, arr1, arr2):
-    for i in range(5):
+    for i in range(n):
         arr1[i] = get_bi_str(arr1[i], n)
         arr2[i] = get_bi_str(arr2[i], n)
-    result = [[0] * n for _ in range(n)]
+    result = ['' * n]
     print(result)
-    for i in range(5):
-        for j in range(5):
+    for i in range(n):
+        for j in range(n):
             if arr1[i][j] == 0 and arr2[i][j] == 0:
-                result[i][j] = ' '
+                result[i] += ' '
             else:
-                result[i][j] = '#'
+                result[i] += '#'
     return result
 
 def get_bi_str(num, n):
