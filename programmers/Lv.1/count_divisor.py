9edbe666451d925377f98279ce99ad2de5ed69cd
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-01 17:14:43 +0900

    Change code checking number's divisor

--- programmers/Lv.1/count_divisor.py
+++ programmers/Lv.1/count_divisor.py
@@ -1,17 +1,11 @@
 # Lv.1
 # 약수의 개수와 덧셈
 def solution(left, right):
-    int_list = [0] * (right - left + 1)
-    # 약수의 개수 구하기
-    for i in range(1, right + 1):
-        for j in range(left, right + 1):
-            if j >= i and j % i == 0:
-                int_list[j - left] += 1
-    # 결괏값 구하기
     answer = 0
-    for i in range(left, right + 1):
-        if int_list[i - left] % 2 == 0:
-            answer += i
+    for num in range(left, right + 1):
+        temp = num ** 0.5
+        if int(temp) == temp:
+            answer -= num
         else:
-            answer -= i
+            answer += num
     return answer
