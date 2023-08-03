41d88ae6cbc569d12d6930e962f67628c7202931
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-03 16:25:20 +0900

    Delete unnecessary blank line

--- programmers/Lv.1/failure_percent.py
+++ programmers/Lv.1/failure_percent.py
@@ -3,7 +3,6 @@
 def solution(N, stages):
     failure_rate = {}
     stages_len = len(stages)
-
     for i in range(1, N+1):
         if stages_len != 0:
             stage_count = stages.count(i)
@@ -11,7 +10,5 @@ def solution(N, stages):
             stages_len -= stage_count # 다음 단계에 도달한 사람들 수
         else:
             failure_rate[i] = 0 # 사람이 없는 단계들은 일괄 처리
-
     answer = sorted(failure_rate.keys(), key=lambda key: failure_rate[key], reverse=True) # 실패율로 정렬
-
     return answer
