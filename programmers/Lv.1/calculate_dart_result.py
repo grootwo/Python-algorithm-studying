39b6645f1899cc08d73883c95eacda2f53200adb
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-04 22:36:34 +0900

    Add second and third condition code

--- programmers/Lv.1/calculate_dart_result.py
+++ programmers/Lv.1/calculate_dart_result.py
@@ -2,10 +2,21 @@ def solution(dartResult):
     answer = ['', '', '']
     check = -1
     for i in dartResult:
+        sqr_dic = {'S': '**1', 'D': '**2', 'T': '**3'}
         # 정수라면
         if '0' <= i <= '9':
             check += 1
             answer[check] += i
+        elif i == 'S' or 'D' or 'T':
+            answer[check] += sqr_dic[i]
+        else:
+            if i == '#':
+                answer[check] += '*-1'
+            elif i == '*' and check > 0:
+                answer[check] += '*2'
+                answer[check - 1] += '*2'
+            else:
+                answer[check] += '*2'
         # 정수인지 확인
         # 정수를 count하며 1, 2, 3에 더하기
         # 정수라면 각 정답에 더하기
