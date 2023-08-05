6b35a57e467acf33f26d74977528ac80ecfc22ac
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-05 22:20:34 +0900

    Debug by printing
    If result is 10, it makes out of range error

--- programmers/Lv.1/calculate_dart_result.py
+++ programmers/Lv.1/calculate_dart_result.py
@@ -4,25 +4,27 @@ def solution(dartResult):
     answer = ['', '', '']
     check = -1
     for i in dartResult:
+        print('i:', i)
+        print('check:', check)
         sqr_dic = {'S': '**1', 'D': '**2', 'T': '**3'}
         # 정수라면
         if '0' <= i <= '9':
+            print('1')
             check += 1
-            answer[check] += i
-        elif i == 'S' or 'D' or 'T':
-            answer[check] += sqr_dic[i]
+            # answer[check] += i
+        # 보너스라면
+        elif i == 'S' or i == 'D' or i == 'T':
+            print('2')
+            # answer[check] += sqr_dic[i]
+        # 옵션이라면
         else:
-            if i == '#':
-                answer[check] += '*-1'
-            elif i == '*' and check > 0:
-                answer[check] += '*2'
-                answer[check - 1] += '*2'
-            else:
-                answer[check] += '*2'
-        # 정수인지 확인
-        # 정수를 count하며 1, 2, 3에 더하기
-        # 정수라면 각 정답에 더하기
-        # SDT에 따라 곱셈 추가
-        # 옵션에 따라 마지막에 곱셈 추가
+            print('3')
+            # if i == '#':
+            #     answer[check] += '*-1'
+            # elif i == '*' and check > 0:
+            #     answer[check] += '*2'
+            #     answer[check - 1] += '*2'
+            # else:
+            #     answer[check] += '*2'
     print(answer)
     return answer
