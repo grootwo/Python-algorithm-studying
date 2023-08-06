76fe490196271485d79abc69fa05794d67ad335c
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-06 16:32:29 +0900

    Change dart data structure to deque
    But error with concatenate str

--- programmers/Lv.1/calculate_dart_result.py
+++ programmers/Lv.1/calculate_dart_result.py
@@ -1,30 +1,41 @@
 # Lv.1
 # [1차] 다트 게임
+from collections import deque
 def solution(dartResult):
     answer = ['', '', '']
+    sqr_dic = {'S': '**1', 'D': '**2', 'T': '**3'}
     check = -1
-    for i in dartResult:
-        print('i:', i)
-        print('check:', check)
-        sqr_dic = {'S': '**1', 'D': '**2', 'T': '**3'}
+    dartResult = deque(dartResult)
+    while dartResult:
         # 정수라면
-        if '0' <= i <= '9':
-            print('1')
+        if dartResult[0] + dartResult[1] == '10':
+            temp = int(dartResult[0] + dartResult[1])
+            dartResult.popleft()
+            dartResult.popleft()
             check += 1
-            # answer[check] += i
-        # 보너스라면
-        elif i == 'S' or i == 'D' or i == 'T':
-            print('2')
-            # answer[check] += sqr_dic[i]
-        # 옵션이라면
+            answer[check] += temp
+        elif '0' <= dartResult[0] <= '9':
+            temp = dartResult[0]
+            dartResult.popleft()
+            check += 1
+            answer[check] += temp
         else:
-            print('3')
-            # if i == '#':
-            #     answer[check] += '*-1'
-            # elif i == '*' and check > 0:
-            #     answer[check] += '*2'
-            #     answer[check - 1] += '*2'
-            # else:
-            #     answer[check] += '*2'
+            dartResult.popleft()
+        if check >= len(dartResult):
+            break
+        # # 보너스라면
+        # elif i == 'S' or i == 'D' or i == 'T':
+        #     print('2')
+        #     answer[check] += sqr_dic[i]
+        # # 옵션이라면
+        # else:
+        #     print('3')
+        #     if i == '#':
+        #         answer[check] += '*-1'
+        #     elif i == '*' and check > 0:
+        #         answer[check] += '*2'
+        #         answer[check - 1] += '*2'
+        #     else:
+        #         answer[check] += '*2'
     print(answer)
     return answer
