0d1659cec59888647af12fd7f697960f4b91fc96
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-07 20:53:12 +0900

    Fix returning range

--- programmers/Lv.1/count_in_budget.py
+++ programmers/Lv.1/count_in_budget.py
@@ -2,8 +2,7 @@ def solution(d, budget):
     if sum(d) <= budget:
         return len(d)
     d.sort()
-    print(d)
     # 모든 부서에 사줄 수 없는 경우도 있나?
-    for i in range(1, len(d)):
-        if len(d[0:i]) > budget:
-            return sum(d[0:i - 1])
+    for i in range(1, len(d) + 1):
+        if sum(d[0:i]) > budget:
+            return i - 1
