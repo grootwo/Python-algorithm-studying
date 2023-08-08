4e4c10e2bfd0e6952f481b4a006fbf81353213be
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-08 23:22:53 +0900

    Add removing code
    With nonetype Error

--- programmers/Lv.1/pick_three_sum_prime.py
+++ programmers/Lv.1/pick_three_sum_prime.py
@@ -3,4 +3,8 @@ def solution(nums):
     nums = sorted(set(sum(i) for i in list(combinations(nums, 3))))
     print(nums)
     count = 0
-    return count
+    for i in range(1, max(nums)):
+        for j in range(len(nums)):
+            if nums[j] % i == 0:
+                nums[j] = None
+    return len(nums)
