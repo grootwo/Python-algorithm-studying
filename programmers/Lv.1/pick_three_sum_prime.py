99629de9e6da6adc930c8f74e918a649f80702df
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-09 23:11:29 +0900

    Change code checking prime numbers
    But with error

--- programmers/Lv.1/pick_three_sum_prime.py
+++ programmers/Lv.1/pick_three_sum_prime.py
@@ -1,10 +1,13 @@
 from itertools import combinations
 def solution(nums):
     nums = sorted(set(sum(i) for i in list(combinations(nums, 3))))
-    print(nums)
+    # print(nums)
     count = 0
     for i in range(1, max(nums)):
         for j in range(len(nums)):
-            if nums[j] % i == 0:
+            if nums[j] and nums[j] > i and nums[j] % i == 0:
+                print(nums[j])
                 nums[j] = None
-    return len(nums)
+    print('len:', len(nums))
+    print('None:', nums.count(None))
+    return len(nums) - nums.count(None)
