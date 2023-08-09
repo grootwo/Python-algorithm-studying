e7903ffaaea33212cd16ff740e97b97ae2db83b8
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-09 23:14:47 +0900

    Delete code printing values

--- programmers/Lv.1/pick_three_sum_prime.py
+++ programmers/Lv.1/pick_three_sum_prime.py
@@ -1,12 +1,8 @@
 from itertools import combinations
 def solution(nums):
     nums = sorted(set(sum(i) for i in list(combinations(nums, 3))))
-    # print(nums)
     for i in range(2, max(nums)):
         for j in range(len(nums)):
             if nums[j] and nums[j] > i and nums[j] % i == 0:
-                print(nums[j])
                 nums[j] = None
-    # print('len:', len(nums))
-    # print('None:', nums.count(None))
     return len(nums) - nums.count(None)
