5cc242eecac31961f4891dbbf2285b9753b8ac7f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-09 23:13:32 +0900

    Change the range that starts from 2
    But error

--- programmers/Lv.1/pick_three_sum_prime.py
+++ programmers/Lv.1/pick_three_sum_prime.py
@@ -2,12 +2,11 @@ from itertools import combinations
 def solution(nums):
     nums = sorted(set(sum(i) for i in list(combinations(nums, 3))))
     # print(nums)
-    count = 0
-    for i in range(1, max(nums)):
+    for i in range(2, max(nums)):
         for j in range(len(nums)):
             if nums[j] and nums[j] > i and nums[j] % i == 0:
                 print(nums[j])
                 nums[j] = None
-    print('len:', len(nums))
-    print('None:', nums.count(None))
+    # print('len:', len(nums))
+    # print('None:', nums.count(None))
     return len(nums) - nums.count(None)
