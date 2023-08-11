13907454450195fbd5d4b499df3b762d91479e82
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-11 22:43:53 +0900

    Update get_mutiple_num.py
    With one runtime error

--- programmers/Lv.1/get_mutiple_num.py
+++ programmers/Lv.1/get_mutiple_num.py
@@ -2,6 +2,7 @@
 # x만큼 간격이 있는 n개의 숫자
 def solution(x, n):
     answer = []
-    for i in range(x, x*n + 1, x):
+    check = 1 if x > 0 else -1
+    for i in range(x, x * n + check, x):
         answer.append(i)
     return answer
