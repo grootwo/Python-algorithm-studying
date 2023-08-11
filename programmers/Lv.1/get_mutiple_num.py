8b39a15b3b109f9a94677914bc7f8bef3a46603b
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-11 22:46:33 +0900

    Edit code about range's index

--- programmers/Lv.1/get_mutiple_num.py
+++ programmers/Lv.1/get_mutiple_num.py
@@ -2,7 +2,6 @@
 # x만큼 간격이 있는 n개의 숫자
 def solution(x, n):
     answer = []
-    check = 1 if x > 0 else -1
-    for i in range(x, x * n + check, x):
-        answer.append(i)
+    for i in range(n):
+        answer.append(x * (i + 1))
     return answer
