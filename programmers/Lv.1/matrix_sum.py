2742779780a11a828e0fee47d8722f6f843e6bbe
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-12 21:56:07 +0900

    Delete code printing answer

--- programmers/Lv.1/matrix_sum.py
+++ programmers/Lv.1/matrix_sum.py
@@ -2,9 +2,7 @@ def solution(arr1, arr2):
     col = len(arr1[0])
     row = len(arr1)
     answer = [[0] * col for _ in range(row)]
-    print(answer)
     for r in range(row):
         for c in range(col):
             answer[r][c] = arr1[r][c] + arr2[r][c]
-    print(answer)
     return answer
