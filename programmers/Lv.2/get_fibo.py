5418fb5a1dc916d6052afc7b44d41310a2c42b29
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-14 23:22:38 +0900

    Create get_fibo.py
    With runtime err

---
+++ programmers/Lv.2/get_fibo.py
@@ -0,0 +1,10 @@
+def solution(n):
+    return get_fibo(n) % 1234567
+
+def get_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return get_fibo(n - 1) + get_fibo(n - 2)
