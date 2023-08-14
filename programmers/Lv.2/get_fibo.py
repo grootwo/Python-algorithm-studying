21068f7863e2d194b0e0a703e3231a01c8d89cf8
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-14 23:31:27 +0900

    Change recursion to array

--- programmers/Lv.2/get_fibo.py
+++ programmers/Lv.2/get_fibo.py
@@ -1,11 +1,7 @@
+# Lv.2
+# 피보나치 수
 def solution(n):
-    n = n % 1234567
-    return get_fibo(n) % 1234567
-
-def get_fibo(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return get_fibo(n - 1) + get_fibo(n - 2)
+    answer = [0, 1]
+    for i in range(2, n + 1):
+        answer.append((answer[i - 1] + answer[i - 2]) % 1234567)
+    return answer[-1]
