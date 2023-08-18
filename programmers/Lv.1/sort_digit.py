e2d43b6319ba1fb5771745d92c513597a3435fa0
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-18 22:08:06 +0900

    Reduce code sorting numbers

--- programmers/Lv.1/sort_digit.py
+++ programmers/Lv.1/sort_digit.py
@@ -1,6 +1,3 @@
 def solution(n):
-    nums = list(int(cha) for cha in str(n))
-    nums.sort()
-    nums.reverse()
-    answer = list(str(num) for num in nums)
-    return int(''.join(answer))
+    nums = sorted(list(str(n)), reverse = True)
+    return int(''.join(nums))
