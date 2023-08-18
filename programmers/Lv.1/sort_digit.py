a3a91a0f7c1197fb28042a43fd10b8cbff5cd4c2
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-18 22:03:57 +0900

    Add code changing str to int

--- programmers/Lv.1/sort_digit.py
+++ programmers/Lv.1/sort_digit.py
@@ -2,4 +2,5 @@ def solution(n):
     nums = list(int(cha) for cha in str(n))
     nums.sort()
     nums.reverse()
-    return ''.join(nums)
+    answer = list(str(num) for num in nums)
+    return int(''.join(answer))
