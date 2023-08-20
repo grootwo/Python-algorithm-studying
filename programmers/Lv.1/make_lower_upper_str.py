2b751d07186f31b6aeb5b6cf7ea69c17d41d510a
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-20 22:37:59 +0900

    Make strings into list but with err
    TypeError: sequence item 0: expected str instance, list found

--- programmers/Lv.1/make_lower_upper_str.py
+++ programmers/Lv.1/make_lower_upper_str.py
@@ -1,9 +1,10 @@
 def solution(s):
-    strings = list(s.split(' '))
-    for string in strings:
-        for i in range(len(string)):
-            if i % 2 == 0: # 홀수 번째 문자
-                string[i] = string[i].lower()
-            else: # 짝수 번째 문자
-                string[i] = string[i].upper()
+    strings = list(map(list, s.split(' ')))
+    print(strings)
+    # for string in strings:
+    #     for i in range(len(string)):
+    #         if i % 2 == 0: # 홀수 번째 문자
+    #             string[i] = string[i].lower()
+    #         else: # 짝수 번째 문자
+    #             string[i] = string[i].upper()
     return ' '.join(strings)
