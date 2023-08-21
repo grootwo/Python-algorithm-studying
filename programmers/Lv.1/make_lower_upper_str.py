891c873e42f511ea259319764c08a6a4534cb7a3
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-21 23:37:41 +0900

    Fix code checking odd numbers

--- programmers/Lv.1/make_lower_upper_str.py
+++ programmers/Lv.1/make_lower_upper_str.py
@@ -3,7 +3,7 @@ def solution(s):
     print(strings)
     for string in strings:
         for i in range(len(string)):
-            if i % 2 == 0: # 홀수 번째 문자
+            if i % 2 == 1: # 홀수 번째 문자
                 string[i] = string[i].lower()
             else: # 짝수 번째 문자
                 string[i] = string[i].upper()
