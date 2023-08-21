053536c3444a86c7cf978ee7430f12188c333a62
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-21 23:35:21 +0900

    Add code mapping list
    With more debugging

--- programmers/Lv.1/make_lower_upper_str.py
+++ programmers/Lv.1/make_lower_upper_str.py
@@ -1,10 +1,12 @@
 def solution(s):
     strings = list(map(list, s.split(' ')))
     print(strings)
-    # for string in strings:
-    #     for i in range(len(string)):
-    #         if i % 2 == 0: # 홀수 번째 문자
-    #             string[i] = string[i].lower()
-    #         else: # 짝수 번째 문자
-    #             string[i] = string[i].upper()
-    return ' '.join(strings)
+    for string in strings:
+        for i in range(len(string)):
+            if i % 2 == 0: # 홀수 번째 문자
+                string[i] = string[i].lower()
+            else: # 짝수 번째 문자
+                string[i] = string[i].upper()
+    answer = map(' '.join, map(''.join, strings))
+    print(answer)
+    return 0
