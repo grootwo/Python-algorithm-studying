2cfaf371c93e94296c90bc8a62396c0bf55fa46d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-21 23:38:48 +0900

    Fix code making answer string

--- programmers/Lv.1/make_lower_upper_str.py
+++ programmers/Lv.1/make_lower_upper_str.py
@@ -1,12 +1,9 @@
 def solution(s):
     strings = list(map(list, s.split(' ')))
-    print(strings)
     for string in strings:
         for i in range(len(string)):
             if i % 2 == 1: # 홀수 번째 문자
                 string[i] = string[i].lower()
             else: # 짝수 번째 문자
                 string[i] = string[i].upper()
-    answer = map(' '.join, map(''.join, strings))
-    print(answer)
-    return 0
+    return ' '.join([''.join(str) for str in strings])
