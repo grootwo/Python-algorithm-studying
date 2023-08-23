c66c9a0c037c57a8f430fc7d9fd6686348136ed1
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-23 17:05:08 +0900

    Edit code checking upper and lower case

--- programmers/Lv.1/make_caesar_cipher.py
+++ programmers/Lv.1/make_caesar_cipher.py
@@ -1,11 +1,16 @@
+# Lv.1
+# 시저 암호
 def solution(s, n):
     answer = []
     for cha in s:
-        if cha != ' ':
-            temp = ord(cha) + n
+        temp = ord(cha)
+        if cha != ' ' and temp < 91: # 대문자의 경우
+            temp += n
+            if temp > 90:
+                temp -= 26
+        elif cha != ' ' and temp > 96: # 소문자의 경우
+            temp += n
             if temp > 122:
                 temp -= 26
-            answer.append(temp)
-        else:
-            answer.append(' ')
+        answer.append(temp) # 띄어쓰기는 그대로
     return ''.join([chr(i) for i in answer])
