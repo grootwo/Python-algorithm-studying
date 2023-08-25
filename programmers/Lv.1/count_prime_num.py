24d881bc7df70f818a33b89d5e2d283bc7285672
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-25 15:08:01 +0900

    Edit code checking a prime number

--- programmers/Lv.1/count_prime_num.py
+++ programmers/Lv.1/count_prime_num.py
@@ -1,11 +1,16 @@
+# Lv.1
+# 소수 찾기
 def solution(n):
-    nums = [i for i in range(1, n + 1)]
     count = 0
-    for num in nums:
-        for i in range(2, int(n ** 0.5) + 1):
-            if i >= num:
-                break
+    for num in range(1, n + 1):
+        count += is_prime(num)
+    return count
+
+def is_prime(num):
+    if num == 0 or num == 1:
+        return 0
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
-                count += 1
-                break
-    return len(nums) - count - 1 # 1 제외
+                return 0
+    return 1
