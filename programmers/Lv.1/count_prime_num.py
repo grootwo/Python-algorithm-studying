22c86468af0b3f46f0e62b760e7cf62d8e1a6c60
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-25 14:59:38 +0900

    Create count_prime_num.py

---
+++ programmers/Lv.1/count_prime_num.py
@@ -0,0 +1,11 @@
+def solution(n):
+    nums = [i for i in range(1, n + 1)]
+    count = 0
+    for num in nums:
+        for i in range(2, n + 1):
+            if i >= num:
+                break
+            if num % i == 0:
+                count += 1
+                break
+    return len(nums) - count - 1
