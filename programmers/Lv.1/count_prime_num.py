33e52d2976cebd4e21ae35a90882c95ebf396139
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-25 15:02:47 +0900

    Change range of checking prime numbers

--- programmers/Lv.1/count_prime_num.py
+++ programmers/Lv.1/count_prime_num.py
@@ -2,10 +2,10 @@ def solution(n):
     nums = [i for i in range(1, n + 1)]
     count = 0
     for num in nums:
-        for i in range(2, n + 1):
+        for i in range(2, int(n ** 0.5) + 1):
             if i >= num:
                 break
             if num % i == 0:
                 count += 1
                 break
-    return len(nums) - count - 1
+    return len(nums) - count - 1 # 1 제외
