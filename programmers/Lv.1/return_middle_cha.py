8a31857099841923254d4d5ada6363de75e63812
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-28 09:35:25 +0900

    Reduce code into one line

--- programmers/Lv.1/return_middle_cha.py
+++ programmers/Lv.1/return_middle_cha.py
@@ -1,7 +1,4 @@
 # Lv.1
 # 가운데 글자 가져오기
 def solution(s):
-    if len(s) % 2 == 0:
-        return s[len(s) // 2 - 1] + s[len(s) // 2]
-    else:
-        return s[len(s) // 2]
+    return s[len(s) // 2 - 1] + s[len(s) // 2] if len(s) % 2 == 0 else s[len(s) // 2]
