87af6093e0fb959cb7bcabefbfad7e54e6e40bc2
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-08-30 10:54:04 +0900

    Edit comments

--- programmers/Lv.1/count_in_budget.py
+++ programmers/Lv.1/count_in_budget.py
@@ -1,8 +1,9 @@
+# Lv.1
+# 예산
 def solution(d, budget):
-    if sum(d) <= budget:
+    if sum(d) <= budget: # 모두 사줄 수 있는 경우
         return len(d)
     d.sort()
-    # 모든 부서에 사줄 수 없는 경우도 있나?
     for i in range(1, len(d) + 1):
         if sum(d[0:i]) > budget:
             return i - 1
