4b0ebdb2bc2f6b568d3c856408607edae2249962
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-03 09:05:54 +0900

    Reduce code into one line

--- programmers/Lv.1/sort_strings_by_index.py
+++ programmers/Lv.1/sort_strings_by_index.py
@@ -1,7 +1,5 @@
 # Lv.1
 # 문자열 내 마음대로 정렬하기
 def solution(strings, n):
-    list_strings = [list(string) for string in strings]
-    list_strings.sort(key=lambda x: (x[n], x)) # 인덱스, 사전순으로 단어 정렬
-    strings = [''.join(list_str) for list_str in list_strings]
+    strings.sort(key=lambda x: (x[n], x))
     return strings
