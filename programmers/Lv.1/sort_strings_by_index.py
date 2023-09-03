1532f37f49e0fe588a026199cd014b28396c10bb
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-03 09:03:42 +0900

    Add a lambda rule to sort string by the dictionary

--- programmers/Lv.1/sort_strings_by_index.py
+++ programmers/Lv.1/sort_strings_by_index.py
@@ -2,6 +2,6 @@
 # 문자열 내 마음대로 정렬하기
 def solution(strings, n):
     list_strings = [list(string) for string in strings]
-    list_strings.sort(key=lambda x: x[n])
+    list_strings.sort(key=lambda x: (x[n], x)) # 인덱스, 사전순으로 단어 정렬
     strings = [''.join(list_str) for list_str in list_strings]
     return strings
