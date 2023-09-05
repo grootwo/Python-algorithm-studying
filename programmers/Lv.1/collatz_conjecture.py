1ab08831a1dc63a31b2868aa286b068a6c8e7273
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-05 10:09:39 +0900

    Create collatz_conjecture.py

---
+++ programmers/Lv.1/collatz_conjecture.py
@@ -0,0 +1,13 @@
+# Lv.1
+# 콜라츠 추측
+def solution(num):
+    count = 0
+    while num != 1:
+        if count == 500:
+            return -1
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        count += 1
+    return count
