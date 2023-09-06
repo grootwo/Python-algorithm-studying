72940f689709d473f95dc03abc8402272e1fa5e5
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-06 11:10:58 +0900

    Add comments

--- programmers/Lv.1/solve_bi_map.py
+++ programmers/Lv.1/solve_bi_map.py
@@ -1,7 +1,7 @@
 # Lv.1
 # [1차] 비밀지도
 def solution(n, arr1, arr2):
-    for i in range(n):
+    for i in range(n): # 각 정수마다 이진수 문자열로 변환
         arr1[i] = get_bi_str(arr1[i], n)
         arr2[i] = get_bi_str(arr2[i], n)
     result = [''] * n
@@ -13,7 +13,7 @@ def solution(n, arr1, arr2):
                 result[i] += '#'
     return result
 
-def get_bi_str(num, n):
+def get_bi_str(num, n): # 십진수를 이진수 문자열로 반환
     answer = ''
     temp = num
     while temp != 0:
