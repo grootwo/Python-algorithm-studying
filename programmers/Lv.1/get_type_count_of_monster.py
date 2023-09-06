b4804ed386ca1ff23927e76e0908ac693e05d6f2
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-06 10:58:25 +0900

    Edit some comment

--- programmers/Lv.1/get_type_count_of_monster.py
+++ programmers/Lv.1/get_type_count_of_monster.py
@@ -6,4 +6,4 @@ def solution(nums):
     if len(count) >= len(nums) // 2: # 폰켓몬의 종류가 데려갈 수 있는 수 이상일 경우
         return len(nums) // 2
     else:
-        return len(count) # 폰켓몬의 종류가 데려살 수 있는 수 미만일 경우
+        return len(count) # 폰켓몬의 종류가 데려갈 수 있는 수 미만일 경우
