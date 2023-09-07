57f7e521ff88acac9354907e31f6bfc0a80af035
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-07 10:10:34 +0900

    Create get_day_of_date.py

---
+++ programmers/Lv.1/get_day_of_date.py
@@ -0,0 +1,26 @@
+def solution(a, b):
+    days_dic = {0: 'FRI', 1: 'SAT', 2: 'SUN', 3: 'MON', 4: 'TUE', 5: 'WED', 6: 'THU'}
+    days = get_days_btw(a, b)
+    answer = days_dic[days % 7]
+    return answer
+
+def get_days_btw(month, day):
+    month_dic = {
+        1: 30,
+        2: 29,
+        3: 31,
+        4: 30,
+        5: 31,
+        6: 30,
+        7: 31,
+        8: 31,
+        9: 30,
+        10: 31,
+        11: 30,
+        12: 31
+    }
+    result = day # 해당 달의 날 더하기
+    for i in range(1, month): # 해당 달 전까지 날 더하기
+        result += month_dic[i]
+    result -= 1 # 1월 1일 제외
+    return result
