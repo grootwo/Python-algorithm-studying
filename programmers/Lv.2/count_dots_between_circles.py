9aa08ad08b4206673d2ad5c17df0961ba75a39ac
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-10 21:22:27 +0900

    Change range and returning value

--- programmers/Lv.2/count_dots_between_circles.py
+++ programmers/Lv.2/count_dots_between_circles.py
@@ -2,11 +2,11 @@ import math
 def solution(r1, r2):
     count = 0
     for i in range(r2 + 1):
-        for j in range(r2 + 1):
+        for j in range(1, r2 + 1):
             if i ** 2 + j ** 2 <= r2 ** 2:
                 count += 1
     # for i in range(r2 + 1):
     #     for j in range(r1 + 1):
     #         count += math.floor(math.sqrt(abs(i ** 2 - j ** 2)))
     #         print(count)
-    return count
+    return count * 4
