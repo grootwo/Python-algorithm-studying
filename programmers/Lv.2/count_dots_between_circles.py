8e3fae6978df7a96cd79950ed6dcccb7e8e9353b
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-11 14:46:22 +0900

    Edit code checking if dots are inside

--- programmers/Lv.2/count_dots_between_circles.py
+++ programmers/Lv.2/count_dots_between_circles.py
@@ -1,12 +1,10 @@
+# Lv.2
+# 두 원 사이의 정수 쌍
 import math
 def solution(r1, r2):
     count = 0
-    for i in range(r2 + 1):
-        for j in range(1, r2 + 1):
-            if i ** 2 + j ** 2 <= r2 ** 2:
-                count += 1
-    # for i in range(r2 + 1):
-    #     for j in range(r1 + 1):
-    #         count += math.floor(math.sqrt(abs(i ** 2 - j ** 2)))
-    #         print(count)
-    return count * 4
+    for i in range(1, r2 + 1):
+        y_max = int((r2 ** 2 - i ** 2) ** 0.5)
+        y_min = 0 if r1 <= i else math.ceil((r1 ** 2 - i ** 2) ** 0.5)
+        count += y_max - y_min + 1
+    return (count) * 4
