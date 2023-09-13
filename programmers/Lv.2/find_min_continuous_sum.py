21071e481be84ecaba6a91be2b57085f6d85ca1e
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-13 16:15:47 +0900

    Add code moving index but with time err

--- programmers/Lv.2/find_min_continuous_sum.py
+++ programmers/Lv.2/find_min_continuous_sum.py
@@ -2,10 +2,16 @@ from itertools import combinations
 def solution(sequence, k):
     if k in sequence: # 만약 해당 수가 있다면
         return [sequence.index(k), sequence.index(k)]
-    # start_i = 0
-    # end_i = 1
-    # answers = []
-    # while start_i <= end_i:
-    #     if sequence[start_i:end_i] == k:
-    #         start_i += 1
+    start_i = 0
+    end_i = 1
+    answers = []
+    while start_i <= end_i:
+        if sum(sequence[start_i:end_i]) == k:
+            start_i += 1
+            answers.append([start_i, end_i])
+        elif sum(sequence[start_i:end_i]) < k:
+            end_i += 1
+        else:
+            start_i += 1
+    print(answers)
     return 0
