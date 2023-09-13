488248653b638d507520c7ec018b90058475f07f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-13 16:13:28 +0900

    Rewrite code and add code ckecking same number

--- programmers/Lv.2/find_min_continuous_sum.py
+++ programmers/Lv.2/find_min_continuous_sum.py
@@ -1,13 +1,11 @@
 from itertools import combinations
 def solution(sequence, k):
-    answer = []
-    check = False
-    for i in range(1, len(sequence)):
-        if check is True:
-            break
-        for j in range(len(sequence) - i + 1):
-            if sum(sequence[j:j + i]) == k:
-                check = True
-                answer = [j, j + i - 1]
-                break
-    return answer
+    if k in sequence: # 만약 해당 수가 있다면
+        return [sequence.index(k), sequence.index(k)]
+    # start_i = 0
+    # end_i = 1
+    # answers = []
+    # while start_i <= end_i:
+    #     if sequence[start_i:end_i] == k:
+    #         start_i += 1
+    return 0
