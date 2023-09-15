1e25706a8a016990568673d8cf0480871a39ab6f
soaringwave <82134672+soaringwave@users.noreply.github.com>
2023-09-15 23:07:26 +0900

    Add other's code in comments

--- programmers/Lv.2/find_min_continuous_sum.py
+++ programmers/Lv.2/find_min_continuous_sum.py
@@ -18,3 +18,20 @@ def solution(sequence, k):
         else:
             start_i += 1
     return answer
+# def solution(sequence, k):
+#     l = r = 0
+#     answer = [0, len(sequence)]
+#     sum = sequence[0]
+
+#     while True:
+#         if sum < k:
+#             r += 1
+#             if r == len(sequence): break
+#             sum += sequence[r]
+#         else:
+#             if sum == k:
+#                 if r-l < answer[1]-answer[0]:
+#                     answer = [l, r]
+#             sum -= sequence[l]
+#             l += 1
+#     return answer
